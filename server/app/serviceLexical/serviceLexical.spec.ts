0bd3e5883a6d7092c61f83f1a7fe58191d8a0f82
Immaybemarc <immaybemarc@hotmail.com>
2018-01-22 17:07:55 -0500

    Test template service lexical

---
+++ server/app/serviceLexical/serviceLexical.spec.ts
@@ -0,0 +1,36 @@
+const assert = require('assert');
+
+it('can send request', (done) => {
+    assert.ok(true);
+    done();
+});
+
+it('can receive word', (done) => {
+    assert.ok(true);
+    done();
+});
+
+it('has correct length', (done) => {
+    assert.ok(true);
+    done();
+});
+
+it('can receive definition', (done) => {
+    assert.ok(true);
+    done();
+});
+
+it('can receive common words', (done) => {
+    assert.ok(true);
+    done();
+});
+
+it('can receive rare words', (done) => {
+    assert.ok(true);
+    done();
+});
+
+it('respects lettre criterias', (done) => {
+    assert.ok(true);
+    done();
+});
