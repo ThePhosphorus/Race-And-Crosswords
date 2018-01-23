41c4976ce7ade565bb5bcbbd1abaa7a94f10a52b
Randy Sab-Roy <randy.sab.roy@gmail.com>
2018-01-23 10:54:00 -0500

    Merge branch 'develop' into feature/presentation-mots-croises

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
-import { FormsModule } from '@angular/forms';
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { GameComponent } from "./game-component/game.component";
@@ -10,8 +10,7 @@ import { RenderService } from "./render-service/render.service";
 import { AppRoutingModule } from "./app-routing.module";
 import { CameraManagerService } from "./camera-manager-service/camera-manager.service";
 import { CrosswordsComponent } from "./crosswords-component/crosswords.component";
-import { PlayermodeComponent } from './playermode-component/playermode.component';
-
+import { PlayermodeComponent } from "./playermode-component/playermode.component";
 
 @NgModule({
     declarations: [
