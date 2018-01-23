50862b781c7dd48cac817aa377b42861bc324a90
Colin Stephenne <colinste666@gmail.com>
2018-01-23 11:33:33 -0500

    Tests template for generationGrille -They all purposely throw an AssertionError Avec Marc

---
+++ server/app/crosswords/generationGrille/generationGrille.spec.ts
@@ -0,0 +1,76 @@
+const assert = require("assert");
+
+describe("Generation de la grille", () => {
+
+    describe("When an empty grid is generated", () => {
+
+        it("should be 10 by 10 ", () => {
+            assert.ok(false);
+        });
+
+        it("should have x% of black squares", () => {
+            assert.ok(false);
+        });
+
+        it("each line/column should contain one or two words", () => {
+            assert.ok(false);
+        });
+
+    });
+
+    describe("When an word is requested", () => {
+
+        it("should accept request", () => {
+            assert.ok(false);
+        });
+
+        it("should receive a word", () => {
+            assert.ok(false);
+        });
+
+        it("should have a definition", () => {
+            assert.ok(false);
+        });
+
+        it("should respect the constraints", () => {
+            assert.ok(false);
+        });
+
+    });
+
+    describe("When an word is cleaned", () => {
+
+        it("should not have accents or special letters", () => {
+            assert.ok(false);
+        });
+
+        it("should not have hyphens or apostrophies", () => {
+            assert.ok(false);
+        });
+
+    });
+
+    describe("When a grid is completed", () => {
+
+        it("should not have multiples of the same word", () => {
+            assert.ok(false);
+        });
+
+    });
+
+    describe("When the generator is called", () => {
+
+        it("should be able to generate an 'easy' grid", () => {
+            assert.ok(false);
+        });
+
+        it("should be able to generate a 'normal' grid", () => {
+            assert.ok(false);
+        });
+
+        it("should be able to generate a 'hard' grid", () => {
+            assert.ok(false);
+        });
+
+    });
+});
