9af6db1469bfe0659c2f254ea2c672762ae9b576
Unknown <thierry.dubois@polymtl.ca>
2018-01-25 22:03:27 -0500

    Rewrote GET example for future reference

--- server/app/crosswords/serviceLexical/serviceLexical.ts
+++ server/app/crosswords/serviceLexical/serviceLexical.ts
@@ -2,6 +2,7 @@ import { Request, Response, NextFunction } from "express";
 import { Message } from "../../../../common/communication/message";
 import "reflect-metadata";
 import { injectable, } from "inversify";
+import * as http from "http";
 
 module ServiceLexicalMicroservice {
 
@@ -15,9 +16,11 @@ module ServiceLexicalMicroservice {
             message.body = "World";
             res.send(JSON.stringify(message));
         }
-        /*public makeQuery(isRare: boolean, lettres: string){
-            let query: string = "https://api.datamuse.com/words?sp=" + ????c????? + "&qe&md=df";
-            let correspondingWords = http.get('GET', query).body;*/
+        public makeQuery(isRare: boolean, lettres: string){
+            let query: string = "https://api.datamuse.com/words?sp=" + lettres + "&qe&md=df";
+            let correspondingWords = http.get(query, res =>{//SOME LOGIC
+                
+            });
 
         }
     }
