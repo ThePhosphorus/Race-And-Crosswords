425962d1ef0ac5922a7744f9dab7ed818dee8ccb
Unknown <immaybemarc@hotmail.com>
2018-01-29 16:42:05 -0500

    Word class added functions
    get/set word and addConstraint

--- server/app/crosswords/grid/word.ts
+++ server/app/crosswords/grid/word.ts
@@ -14,4 +14,43 @@ export class Word {
     public wordString: string;
     public definitions: string[];
     public orientation: Orientation;
+
+    public Word(orientation: Orientation, position: Position, length: number): void {
+        this.orientation = orientation;
+        this.position = position;
+        for (let i: number = 0; i < length ; i++) {
+            this.wordString += "?";
+        }
+    }
+
+    public setWord(word: string): void {
+        this.wordString = word;
+    }
+
+    public getWord(): string {
+        return this.wordString;
+    }
+
+    public addLetterConstraint(position: number, letter: string): boolean {
+        if (letter.length !== 1) {
+            return false;
+        }
+        let tempString: string;
+
+        for (let i: number = 0; i < position ; i++) {
+            tempString += this.wordString[i];
+        }
+
+        tempString += letter;
+
+        for (let i: number = position + 1; i < this.wordString.length ; i++) {
+            tempString += this.wordString[i];
+        }
+
+        this.wordString = tempString;
+
+        return true;
+
+    }
+
 }
