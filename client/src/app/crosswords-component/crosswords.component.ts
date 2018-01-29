c4e22ee923c7a2075fe2be6c55473e59790ee1e1
Randy Sab-Roy <randy.sab.roy@gmail.com>
2018-01-29 11:32:01 -0500

    Change td span to input text and validate user input

--- client/src/app/crosswords-component/crosswords.component.ts
+++ client/src/app/crosswords-component/crosswords.component.ts
@@ -1,30 +1,43 @@
 import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: "app-crosswords",
-  templateUrl: "./crosswords.component.html",
-  styleUrls: ["./crosswords.component.css"]
+    selector: "app-crosswords",
+    templateUrl: "./crosswords.component.html",
+    styleUrls: ["./crosswords.component.css"]
 })
 export class CrosswordsComponent implements OnInit {
 
-  public constructor() { }
+    public constructor() { }
 
-  public words: string[][] = [["-", "b", "c", "d", "e", "f", "g", "h", "i", "j"],
-                              ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"],
-                              ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"],
-                              ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"],
-                              ["a", "b", "c", "d", "e", "-", "g", "h", "i", "j"],
-                              ["a", "b", "c", "d", "e", "f", "g", "h", "-", "j"],
-                              ["a", "-", "c", "d", "e", "f", "g", "h", "i", "j"],
-                              ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"],
-                              ["a", "b", "c", "d", "e", "f", "-", "h", "i", "j"],
-                              ["a", "b", "c", "d", "e", "f", "-", "h", "i", "j"],
-                              ];
-  public definitions: string [][] = [["Across definition", "Down definition"],
-                                     ["Across definition", "Down definition"],
-                                     ["", "Down definition"]];
+    public words: string[][] = [["-", "b", "c", "d", "e", "f", "g", "h", "i", "j"],
+                                ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"],
+                                ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"],
+                                ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"],
+                                ["a", "b", "c", "d", "e", "-", "g", "h", "i", "j"],
+                                ["a", "b", "c", "d", "e", "f", "g", "h", "-", "j"],
+                                ["a", "-", "c", "d", "e", "f", "g", "h", "i", "j"],
+                                ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"],
+                                ["a", "b", "c", "d", "e", "f", "-", "h", "i", "j"],
+                                ["a", "b", "c", "d", "e", "f", "-", "h", "i", "j"],
+                                ];
+    public definitions: string[][] = [["Across definition", "Down definition"],
+                                      ["Across definition", "Down definition"],
+                                      ["", "Down definition"]];
 
-  public ngOnInit(): void {
-  }
+    public ngOnInit(): void {
+    }
+
+    public validateInput(event: KeyboardEvent): void {
+        const letters: RegExp = /^[A-Za-z]+$/;
+        const lowerCase: RegExp = /^[a-z]+$/;
+        const inputChar: string = String.fromCharCode(event.charCode);
+
+        if (!letters.test(inputChar)) {
+            event.preventDefault();
+        }
+        if (lowerCase.test(inputChar)) {
+            event.target.value = inputChar.toUpperCase();
+        }
+    }
 
 }
