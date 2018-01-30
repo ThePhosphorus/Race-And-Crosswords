ac8c94eeebeec9dde63aac09e27b1c8545650f0a
Colin Stephenne <colinste666@gmail.com>
2018-01-30 12:03:31 -0500

    Moved grid classes in Common

--- common/communication/crossword-grid.ts
+++ common/communication/crossword-grid.ts
@@ -1,3 +1,79 @@
-export class CrosswordGrid {
+export class Position {
+    public constructor(public column: number, public row: number) {}
+}
 
+export enum Orientation {
+    Vertical = "Vertical",
+    Horizontal = "Horizontal",
+}
+
+export class Word {
+
+    private position: Position;
+    private wordString: string;
+    private definitions: string[];
+    private orientation: Orientation;
+
+    public constructor(orientation: Orientation, position: Position, length: number) {
+        this.orientation = orientation;
+        this.position = position;
+        for (let i: number = 0; i < length ; i++) {
+            this.wordString += "?";
+        }
+    }
+
+    public getPosition(): Position {
+        return this.position;
+    }
+
+    public getOrientation(): Orientation {
+        return this.orientation;
+    }
+
+    public getDefinitions(): string[] {
+        return this.definitions;
+    }
+
+    public addDefinitions(definitions: string[] ): void {
+        definitions.forEach((definition: string) => {
+           this.definitions.push(definition);
+        });
+    }
+
+    public setWord(word: string): void {
+        this.wordString = word;
+    }
+
+    public getWord(): string {
+        return this.wordString;
+    }
+
+    public addLetterConstraint(position: number, letter: string): boolean {
+        if (letter.length !== 1) {
+            return false;
+        }
+        let tempString: string;
+
+        for (let i: number = 0; i < position ; i++) {
+            tempString += this.wordString[i];
+        }
+
+        tempString += letter;
+
+        for (let i: number = position + 1; i < this.wordString.length ; i++) {
+            tempString += this.wordString[i];
+        }
+
+        this.wordString = tempString;
+
+        return true;
+
+    }
+
+}
+
+export class CrosswordGrid {
+    public blackTiles: Position[];
+    public across: Word[][];
+    public down: Word[][];
 }
