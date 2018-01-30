31e487cdb5f3ef45e5c773bce9e17a77aa4ce621
Randy Sab-Roy <randy.sab.roy@gmail.com>
2018-01-30 11:31:16 -0500

    Migrate to request-promise-native

--- server/app/crosswords/lexical/datamuse.ts
+++ server/app/crosswords/lexical/datamuse.ts
@@ -1,32 +1,18 @@
-import * as http from "http";
-import { IncomingMessage } from "https";
+import * as request from "request-promise-native";
 import { Word } from "../../../../common/communication/word";
 
-const HTTP_STATUS_OK: number = 200;
 const HARD_THRESHOLD: number = 1000;
 
 export class Datamuse {
     public getWords(constraint: string, callback: (words: Array<Word>) => void): void {
-        http.get("http://api.datamuse.com/words?sp=" + constraint + "&md=def", (res: IncomingMessage) => {
-            const { statusCode } = res;
-
-            if (statusCode === HTTP_STATUS_OK) {
-                res.setEncoding("utf8");
-                let rawData: string = "";
-
-                res.on("data", (chunk: string | Buffer) => { rawData += chunk; });
-                res.on("end", () => {
-                    try {
-                        const parsedWords: Array<Word> = JSON.parse(rawData);
-                        callback(parsedWords);
-                    } catch (e) {
-                        callback([]);
-                    }
-                });
-            } else {
+        request("http://api.datamuse.com/words?sp=" + constraint + "&md=def")
+            .then((htmlString: string) => {
+                const parsedWords: Array<Word> = JSON.parse(htmlString);
+                callback(parsedWords);
+            })
+            .catch(() => {
                 callback([]);
-            }
-        });
+            });
     }
 
     public getEasyWord(constraint: string, callback: (word: Word) => void): void {
