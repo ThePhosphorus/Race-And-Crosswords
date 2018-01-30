73c1024ffb3ee1ea04c320cfe650073704a7a2f7
Adem Aber Aouni <ademaberaouni@gmail.com>
2018-01-30 12:18:08 -0500

    Better steering Limitation
    Avec Thierry Dubois

--- client/src/app/car/car.ts
+++ client/src/app/car/car.ts
@@ -20,7 +20,7 @@ const INITIAL_WEIGHT_DISTRIBUTION: number = 0.5;
 const MINIMUM_SPEED: number = 0.05;
 const NUMBER_REAR_WHEELS: number = 2;
 const NUMBER_WHEELS: number = 4;
-const APPROX_MAXIMUM_SPEED: number=220;
+const APPROX_MAXIMUM_SPEED: number = 220;
 const METER_TO_KM_SPEED_CONVERSION: number = 3.6;
 
 export class Car extends Object3D {
@@ -37,7 +37,7 @@ export class Car extends Object3D {
     private mesh: Object3D;
     private steeringWheelDirection: number;
     private weightRear: number;
-    private maximum_speed: number;
+    private steeringWheelState: number;
 
     public get speed(): Vector3 {
         return this._speed.clone();
@@ -100,9 +100,8 @@ export class Car extends Object3D {
         this.weightRear = INITIAL_WEIGHT_DISTRIBUTION;
         this._speed = new Vector3(0, 0, 0);
 
-        this.maximum_speed = APPROX_MAXIMUM_SPEED;
+        this.steeringWheelState = 0;
 
-        
     }
 
     // TODO: move loading code outside of car class.
@@ -125,15 +124,20 @@ export class Car extends Object3D {
     }
 
     public steerLeft(): void {
-        this.steeringWheelDirection = MAXIMUM_STEERING_ANGLE * (this.maximum_speed-(this._speed.length()*METER_TO_KM_SPEED_CONVERSION))/this.maximum_speed;
+        this.steeringWheelState = 1;
     }
 
     public steerRight(): void {
-        this.steeringWheelDirection = -MAXIMUM_STEERING_ANGLE * (this.maximum_speed-(this._speed.length()*METER_TO_KM_SPEED_CONVERSION))/this.maximum_speed;
+        this.steeringWheelState = -1;
+    }
+
+    private updateSteering(): void {
+        this.steeringWheelDirection = this.steeringWheelState *
+        MAXIMUM_STEERING_ANGLE * (APPROX_MAXIMUM_SPEED - (this._speed.length() * METER_TO_KM_SPEED_CONVERSION)) / APPROX_MAXIMUM_SPEED;
     }
 
     public releaseSteering(): void {
-        this.steeringWheelDirection = 0;
+        this.steeringWheelState = 0;
     }
 
     public releaseBrakes(): void {
@@ -160,6 +164,7 @@ export class Car extends Object3D {
         // Move back to world coordinates
         this._speed = this.speed.applyQuaternion(rotationQuaternion.inverse());
 
+        this.updateSteering();
         // Angular rotation of the car
         const R: number =
             DEFAULT_WHEELBASE /
@@ -218,14 +223,13 @@ export class Car extends Object3D {
 
         // tslint:disable-next-line:no-magic-numbers
         const rollingCoefficient: number =
-            1 /tirePressure *
+            1 / tirePressure *
                 (Math.pow(this.speed.length() * 3.6 / 100, 2) * 0.0095 + 0.01) + 0.005;
 
-        if (this.isGoingForward())
-        {
+        if (this.isGoingForward()) {
             return this.direction.multiplyScalar(rollingCoefficient * this.mass * GRAVITY);
         }
-        return this.direction.multiplyScalar(-1*rollingCoefficient * this.mass * GRAVITY);
+        return this.direction.multiplyScalar(-1 * rollingCoefficient * this.mass * GRAVITY);
     }
 
     private getDragForce(): Vector3 {
@@ -309,7 +313,4 @@ export class Car extends Object3D {
         return this.mesh.position;
     }
 
-
-
-    
 }
