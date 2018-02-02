e7f9e3088de7a512e78f1cb9bfbe374b62c67400
Elisabeth <elisabeth.lafleur.mtl@gmail.com>
2018-02-02 11:09:36 -0500

    Home-page routing and buttons with working link

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,8 +2,11 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { GameComponent } from "./game-component/game.component";
 import { CrosswordsComponent } from "./crosswords-component/crosswords.component";
+import { HomePageComponent } from "./home-page/home-page.component";
 
 const routes: Routes = [
+  { path: '' , redirectTo: "/home-page", pathMatch: 'full' },
+  { path : "home-page", component: HomePageComponent},
   { path: "race", component: GameComponent },
   { path: "crosswords", component: CrosswordsComponent }
 ];
