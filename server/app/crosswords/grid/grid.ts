b363f5be4d8cac3f9b7be261032f0ea9b575cc18
Marc Anhoury <immaybemarc@hotmail.com>
2018-02-03 12:33:01 -0500

    Removed injetcable from gridGenerator

--- server/app/crosswords/grid/grid.ts
+++ server/app/crosswords/grid/grid.ts
@@ -1,16 +1,19 @@
 import { Request, Response, NextFunction, Router } from "express";
 import { injectable, inject } from "inversify";
 import { WebService } from "../../webServices";
-import { Difficulty, GridGenerator } from "./grid-generator";
-import types from "../../types";
+import {GridGenerator } from "./grid-generator";
+import {Difficulty} from "../../../../common/communication/crossword-grid"
 
 const MIN_GRID_SIZE: number = 2;
 
 @injectable()
 export class Grid extends WebService {
 
-    constructor( @inject(types.GridGenerator) private gridGenerator: GridGenerator) {
+    private gridGenerator: GridGenerator;
+
+    constructor() {
         super();
+        this.gridGenerator=new GridGenerator();
         this._routerName = "/grid";
     }
 
