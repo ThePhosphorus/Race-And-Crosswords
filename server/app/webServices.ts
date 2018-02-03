44811fd1027ee3d525171351a3b8072f30341eec
Adem Aber Aouni <ademaberaouni@gmail.com>
2018-02-03 14:19:33 -0500

    Perfectly working Working routes

--- server/app/webServices.ts
+++ server/app/webServices.ts
@@ -6,22 +6,30 @@ export abstract class WebService {
 
     protected _router: Router;
     private _routeName: string;
+    private _routingDefined: boolean;
 
     public constructor() {
         this._router = Router();
         this._routeName = "/";
+        this._routingDefined = false;
     }
 
     public set routeName(routeName: string) { this._routeName = routeName; }
 
     public get routeName(): string { return this._routeName; }
 
-    public get routes(): Router { return this._router; }
+    public get routes(): Router {
+         if (!this._routingDefined) {
+          this.defineRoutes();
+          this._routingDefined = true;
+         }
+
+         return this._router;
+        }
 
     protected abstract defineRoutes(): void;
 
-    public addSubService(subService: WebService): void
-    {
+    public addSubService(subService: WebService): void {
         this._router.use(subService._routeName, subService._router);
     }
 }
