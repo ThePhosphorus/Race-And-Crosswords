9911e3d7b7045b0befefa94954ef27a20291edde
Adem Aber Aouni <ademaberaouni@gmail.com>
2018-02-04 12:38:23 -0500

    Fixed tests crash

--- client/src/app/crosswords-component/crosswords.component.spec.ts
+++ client/src/app/crosswords-component/crosswords.component.spec.ts
@@ -1,18 +1,20 @@
-import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { CrosswordsComponent } from "./crosswords.component";
+import { CrosswordCommunicationService } from "../crossword-communication-service/crossword.communication.service";
 import { PlayermodeComponent } from "../playermode-component/playermode.component";
 
 describe("CrosswordsComponent", () => {
   let component: CrosswordsComponent;
   let fixture: ComponentFixture<CrosswordsComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ CrosswordsComponent, PlayermodeComponent ]
+      declarations: [ CrosswordsComponent, PlayermodeComponent ],
+      providers: [ CrosswordCommunicationService ]
     })
     .compileComponents();
-  }));
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CrosswordsComponent);
