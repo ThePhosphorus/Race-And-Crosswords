417e3a3f666aed8a3b4c79efcf4c577128c994eb
Adem Aber Aouni <ademaberaouni@gmail.com>
2018-02-04 13:52:37 -0500

    Observable logic

--- client/src/app/crossword-service/crossword.service.spec.ts
+++ client/src/app/crossword-service/crossword.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed, inject } from "@angular/core/testing";
 
 import { CrosswordService } from "./crossword.service";
+import { Difficulty, CrosswordGrid } from "../../../../common/communication/crossword-grid";
 
 describe("CrosswordService", () => {
   beforeEach(() => {
@@ -11,5 +12,11 @@ describe("CrosswordService", () => {
 
   it("should be created", inject([CrosswordService], (service: CrosswordService) => {
     expect(service).toBeTruthy();
+  }));
+
+  it("should recieve a promise", inject([CrosswordService], (service: CrosswordService) => {
+      service.newGame(Difficulty.Easy, 4, 0.3).subscribe( (grid: CrosswordGrid) => {
+        expect(grid).toBeDefined();
+      });
   }));
 });
--- client/src/app/crossword-service/crossword.service.ts
+++ client/src/app/crossword-service/crossword.service.ts
@@ -1,8 +1,45 @@
 import { Injectable } from "@angular/core";
+import { Difficulty, CrosswordGrid } from "../../../../common/communication/crossword-grid";
+import { CrosswordCommunicationService } from "../crossword-communication-service/crossword.communication.service";
+import { Observable } from "rxjs/Observable";
+import "rxjs/add/observable/of";
+
+const STARTING_GRID_SIZE: number = 4;
+const STARTING_BLACK_TILE_RATIO: number = 0.3;
 
 @Injectable()
 export class CrosswordService {
+    private _diff: Difficulty;
+    private _gridSize: number;
+    private _blackTilesRatio: number;
 
-  public constructor() { }
+    private _currentGrid: CrosswordGrid;
 
+    public constructor(private commService: CrosswordCommunicationService) {
+        this._diff = Difficulty.Easy;
+        this._gridSize = STARTING_GRID_SIZE;
+        this._blackTilesRatio = STARTING_BLACK_TILE_RATIO;
+        this._currentGrid = null;
+     }
+
+    public get difficulty(): Difficulty {return this._diff; }
+    public get gridSize(): number {return this._gridSize; }
+    public get blackTileRatio(): number {return this._blackTilesRatio; }
+
+    public get currentGrid(): CrosswordGrid {
+        return this._currentGrid ;
+     }
+
+    public newGame(difficulty: Difficulty, gridSize: number, btRatio: number): Observable<CrosswordGrid> {
+        this._diff = difficulty;
+        this._gridSize = gridSize;
+        this._blackTilesRatio = btRatio;
+        this._currentGrid = null;
+        const request: Observable<CrosswordGrid> = this.commService.getCrossword(difficulty, gridSize, btRatio);
+        request.subscribe((grid: CrosswordGrid) => {
+            this._currentGrid = grid;
+        });
+
+        return request;
+    }
 }
