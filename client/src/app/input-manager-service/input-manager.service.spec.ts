92322778d5f29584d193759e8fd2a98b02455b3e
Adem Aber Aouni <ademaberaouni@gmail.com>
2018-02-04 12:23:45 -0500

    Added Import in test file of input manager

--- client/src/app/input-manager-service/input-manager.service.spec.ts
+++ client/src/app/input-manager-service/input-manager.service.spec.ts
@@ -2,11 +2,12 @@ import { TestBed, inject } from "@angular/core/testing";
 
 import { InputManagerService } from "./input-manager.service";
 import { CameraManagerService } from "../camera-manager-service/camera-manager.service";
+import { RenderService } from "../render-service/render.service";
 
 describe("InputManagerService", () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [InputManagerService, CameraManagerService]
+      providers: [InputManagerService, CameraManagerService, RenderService]
     });
   });
 
