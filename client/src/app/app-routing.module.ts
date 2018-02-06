af723d62375077c3cad24817ed6c097d3d3a2c51
Unknown <immaybemarc@hotmail.com>
2018-02-06 11:04:58 -0500

    Added admin routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from "@angular/router";
 import { GameComponent } from "./game-component/game.component";
 import { CrosswordsComponent } from "./crosswords-component/crosswords.component";
 import { HomePageComponent } from "./home-page/home-page.component";
+import { AdminComponent } from "./admin/admin.component";
 
 const routes: Routes = [
   { path: "" , redirectTo: "/home-page", pathMatch: "full" },
   { path : "home-page", component: HomePageComponent},
   { path: "race", component: GameComponent },
-  { path: "crosswords", component: CrosswordsComponent }
+  { path: "crosswords", component: CrosswordsComponent },
+  { path: "admin", component: AdminComponent }
 ];
 
 @NgModule({
