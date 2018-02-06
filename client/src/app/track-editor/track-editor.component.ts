ae85ef67b1dcd912a66243a87bab420ee020dd01
Adem Aber Aouni <ademaberaouni@gmail.com>
2018-02-06 14:43:55 -0500

    Track editor start

--- client/src/app/track-editor/track-editor.component.ts
+++ client/src/app/track-editor/track-editor.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener } from "@angular/core";
+import { AfterViewInit } from "@angular/core/src/metadata/lifecycle_hooks";
+import { ViewChild } from "@angular/core/src/metadata/di";
+import { TrackGeneratorService } from "../track-generator-service/track.generator.service";
+import { CameraManagerService } from "../camera-manager-service/camera-manager.service";
 
 @Component({
-  selector: 'app-track-editor',
-  templateUrl: './track-editor.component.html',
-  styleUrls: ['./track-editor.component.css']
+  selector: "app-track-editor",
+  templateUrl: "./track-editor.component.html",
+  styleUrls: ["./track-editor.component.css"],
+  providers: [TrackGeneratorService, CameraManagerService]
 })
-export class TrackEditorComponent implements OnInit {
+export class TrackEditorComponent implements AfterViewInit {
+    @ViewChild("editor")
+    private elem: ElementRef;
 
-  constructor() { }
+    public constructor(private trackGen: TrackGeneratorService) { }
 
-  ngOnInit() {
-  }
+    public ngAfterViewInit(): void {
+        this.trackGen.init(this.elem.nativeElement);
+     }
+
+    @HostListener("window:resize", ["$event"])
+    public onResize(): void {
+        this.trackGen.onResize();
+    }
+
+    @HostListener("window:keydown", ["$event"])
+    public onKeyDown(event: KeyboardEvent): void {
+    this.trackGen.InputkeyDown(event);
+    }
+
+    @HostListener("window:keyup", ["$event"])
+    public onKeyUp(event: KeyboardEvent): void {
+    this.trackGen.InputKeyUp(event);
+    }
 
 }
--- client/src/app/track-generator-service/track.generator.service.ts
+++ client/src/app/track-generator-service/track.generator.service.ts
@@ -1,8 +1,35 @@
 import { Injectable } from "@angular/core";
+import { TrackRenderer } from "./track.generator.renderer";
+import { CameraManagerService } from "../camera-manager-service/camera-manager.service";
 
 @Injectable()
 export class TrackGeneratorService {
+    private _renderer: TrackRenderer;
 
-  public constructor() { }
+    public constructor(private cameraService: CameraManagerService) {
+     }
 
+    public init(div: HTMLDivElement): void {
+       this._renderer = new TrackRenderer(div, this.cameraService);
+     }
+
+    public InputkeyDown(event: KeyboardEvent): void {
+
+    }
+
+    public InputKeyUp(event: KeyboardEvent): void {
+
+    }
+
+    public mouseEventHover(event: MouseEvent): void {
+
+    }
+
+    public mouseEventclick(event: MouseEvent): void {
+
+    }
+
+    public onResize(): void {
+
+    }
 }
