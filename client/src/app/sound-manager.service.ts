504dabd591e28f52ee186fff90b713d93eef2e5b
Unknown <thierry.dubois@polymtl.ca>
2018-02-06 11:25:10 -0500

    Added template load sound function (temp)

--- client/src/app/sound-manager.service.ts
+++ client/src/app/sound-manager.service.ts
@@ -12,4 +12,27 @@ export class SoundManagerService {
 
   constructor() { }
 
+  private loadSounds(): void {
+    const tmpIdle: Audio = this.idleSound = new Audio(this.cameraManager.listener);
+    const tmpAccel: Audio = this.accelSound = new Audio(this.cameraManager.listener);
+
+    const idleLoader: AudioLoader = new AudioLoader();
+    idleLoader.load("../../assets/sounds/idle.ogg",
+                    (buffer: AudioBuffer) => {
+                        tmpIdle.setBuffer(buffer);
+                        tmpIdle.setLoop(true);
+                        tmpIdle.setVolume(0.5);
+                    },
+                    () => { },
+                    () => { });
+    const accelLoader: AudioLoader = new AudioLoader();
+    accelLoader.load("../../assets/sounds/accel.ogg",
+                     (buffer: AudioBuffer) => {
+                        tmpAccel.setBuffer(buffer);
+                        tmpAccel.setLoop(false);
+                        tmpAccel.setVolume(0.5);
+                     },
+                     () => { },
+                     () => { });
+}
 }
