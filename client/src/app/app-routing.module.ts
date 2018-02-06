db79645872d3abbc825307f58e7bcf15c1ebf01e
Randy Sab-Roy <randy.sab.roy@gmail.com>
2018-02-06 12:03:37 -0500

    Add track editor component

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from "@angular/router";
 import { GameComponent } from "./game-component/game.component";
 import { CrosswordsComponent } from "./crosswords-component/crosswords.component";
 import { HomePageComponent } from "./home-page/home-page.component";
+import { TrackEditorComponent } from "./track-editor/track-editor.component";
 
 const routes: Routes = [
   { path: "" , redirectTo: "/home-page", pathMatch: "full" },
   { path : "home-page", component: HomePageComponent},
   { path: "race", component: GameComponent },
-  { path: "crosswords", component: CrosswordsComponent }
+  { path: "crosswords", component: CrosswordsComponent },
+  { path: "track-editor", component: TrackEditorComponent}
 ];
 
 @NgModule({
