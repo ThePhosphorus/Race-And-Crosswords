323660702727c16b76f418ea714e534cad25146c
Unknown <thierry.dubois@polymtl.ca>
2018-02-06 11:48:20 -0500

    injected camera manager in sound service

--- client/src/app/sound-manager.service.ts
+++ client/src/app/sound-manager.service.ts
@@ -7,26 +7,27 @@ import {
     AudioBuffer,
     AudioListener
 } from "three";
+import { CameraManagerService } from "./camera-manager-service/camera-manager.service";
 
 @Injectable()
 export class SoundManagerService {
 
-  constructor() {
-   }
+    public constructor() {
+    private cameraManager: CameraManagerService;
+}
 
-  private idleSound: Audio;
-
-  private loadSounds(): void {
+    private idleSound: Audio;
+    private loadSounds(): void {
     const tmpIdle: Audio = this.idleSound = new Audio(this.cameraManager.listener);
 
     const idleLoader: AudioLoader = new AudioLoader();
     idleLoader.load("../../assets/sounds/idle.ogg",
-                    (buffer: AudioBuffer) => {
-                        tmpIdle.setBuffer(buffer);
-                        tmpIdle.setLoop(true);
-                        tmpIdle.setVolume(0.5);
-                    },
-                    () => { },
-                    () => { });
+        (buffer: AudioBuffer) => {
+            tmpIdle.setBuffer(buffer);
+            tmpIdle.setLoop(true);
+            tmpIdle.setVolume(0.5);
+        },
+        () => { },
+        () => { });
     }
 }
