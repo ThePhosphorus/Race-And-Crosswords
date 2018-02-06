113c1c963273573cd4ee091007af4dacf80c0d01
Unknown <thierry.dubois@polymtl.ca>
2018-02-06 11:23:23 -0500

    added audio imports from threejs

--- client/src/app/sound-manager.service.ts
+++ client/src/app/sound-manager.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from "@angular/core";
+import {
+    Object3D,
+    ObjectLoader,
+    Audio,
+    AudioLoader,
+    AudioBuffer
+} from "three";
 
 @Injectable()
 export class SoundManagerService {
