e833f2f8575ef49531b86a32ee2f207431d7f7dd
Randy Sab-Roy <randy.sab.roy@gmail.com>
2018-02-07 16:59:45 -0500

    Add intersection test

--- client/src/app/track-generator-service/constraint-validator.ts
+++ client/src/app/track-generator-service/constraint-validator.ts
@@ -3,7 +3,7 @@ import { Vector3 } from "three";
 const MINIMUM_ANGLE: number = 0.785398;
 const MINIMUM_LENGTH: number = 5;
 
-class ConstraintValidator {
+export class ConstraintValidator {
     public static validateLine(index1: number, index2: number, points: Array<Vector3>): boolean {
         const line: Vector3 = points[index2].sub(points[index1]);
         const previousLine: Vector3 = points[index1 - 1] ? points[index1].sub(points[index1 - 1]) : null;
@@ -11,7 +11,8 @@ class ConstraintValidator {
 
         return this.validateLength(line)
             && this.validateAngle(previousLine, line)
-            && this.validateAngle(line, nextLine);
+            && this.validateAngle(line, nextLine)
+            && this.validateIntersection(index1, index2, points);
     }
 
     private static validateLength(v: Vector3): boolean {
@@ -19,6 +20,31 @@ class ConstraintValidator {
     }
 
     private static validateAngle(v1: Vector3, v2: Vector3): boolean {
-        return (v1 != null && v2 != null) && v1.angleTo(v2) > MINIMUM_ANGLE;
+        return (v1 == null && v2 == null) || v1.angleTo(v2) > MINIMUM_ANGLE;
+    }
+
+    public static validateIntersection(index1: number, index2: number, points: Array<Vector3>): boolean {
+        const line: Vector3 = points[index2].sub(points[index1]);
+        for (let i: number = 0; i < points.length - 1; i++) {
+            if (points[i + 1] != null && i !== index1 && i + 1 !== index2) {
+                const compare: Vector3 = points[i + 1].sub(points[i]);
+                const denominator: number = (compare.z * line.x) - (compare.x * line.z);
+                if (denominator === 0) {
+                    return false;
+                }
+                let a: number = points[index1].z - points[i].z;
+                let b: number = points[index1].x - points[i].x;
+                const numerator1: number = ((points[i + 1].x - points[i].x) * a) - ((points[i + 1].z - points[i].z) * b);
+                const numerator2: number = ((points[index2].x - points[index1].x) * a) - ((points[index2].z - points[index1].z) * b);
+                a = numerator1 / denominator;
+                b = numerator2 / denominator;
+
+                if (a > 0 && a < 1 && b > 0 && b < 1) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 }
