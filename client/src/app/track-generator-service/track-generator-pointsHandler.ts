859980b52d7ae2888c154438a8b7b5d443fc251a
Adem Aber Aouni <ademaberaouni@gmail.com>
2018-02-10 18:24:56 -0500

    REFACTOR : Track generator pointsHandler

--- client/src/app/track-generator-service/track-generator-pointsHandler.ts
+++ client/src/app/track-generator-service/track-generator-pointsHandler.ts
@@ -23,10 +23,6 @@ export class TrackGeneratorPointsHandler {
         return this._points;
     }
 
-    public point(index: number): Mesh {
-        return this._points[index];
-    }
-
     public get PositionSelectPoints(): C.PosSelect[] {
         const result: C.PosSelect[] = [];
         for (let i: number = 0 ; i < this.length; i++) {
@@ -36,45 +32,40 @@ export class TrackGeneratorPointsHandler {
         return result;
     }
 
-    private toVector2(v: Vector3): Vector2 {
-        return new Vector2(v.x, v.z);
-    }
-
-    public selectPoint(pointId: number): void {
-        if (this._selectedPoint !== -1 && this._selectedPoint !== 0) {
-            this.point(this._selectedPoint).material = C.WHITE_MATERIAL;
-        }
-
-        this._selectedPoint = pointId;
-        this.point(pointId).material = C.SELECTION_MATERIAL;
-
-        if (pointId === 0 && !this.top.position.equals(this._points[0].position)) {
-            this.closeLoop();
-        }
-    }
-
-    private closeLoop(): void {
-        this._points.push(this.trackGen.createDot(
-            this.trackGen.getClientPosition(this._points[0].position),
-            this.top.position));
-    }
-
-    public removePoint(index: number): void {
-        this.trackGen.removeObject(this._points[index], this._points[index - 1], this._points[index + 1]);
-        this._points.splice(index, 1);
-        if (index === this._selectedPoint) {
-            this._selectedPoint = -1;
-        } else if (index < this._selectedPoint) {
-            this._selectedPoint--;
-        }
+    public get selectedPointId(): number {
+        return this._selectedPoint;
     }
 
     public pointSelected(): boolean {
         return this._selectedPoint !== -1;
     }
 
-    public get selectedPointId(): number {
-        return this._selectedPoint;
+    public point(index: number): Mesh {
+        return this._points[index];
+    }
+
+    public selectPoint(pointId: number): void {
+        if ( this._selectedPoint > 0) {
+            this.point(this._selectedPoint).material = C.WHITE_MATERIAL;
+        }
+        if (pointId === 0 && !this.top.position.equals(this._points[0].position)) {
+            this.closeLoop();
+        }
+
+        this._selectedPoint = pointId;
+        this.point(pointId).material = C.SELECTION_MATERIAL;
+
+    }
+
+    public removePoint(index: number): void {
+        if (index === this._selectedPoint) {
+            this._selectedPoint = -1;
+        } else if (index < this._selectedPoint) {
+            this._selectedPoint--;
+        }
+
+        this.trackGen.removeObject(this._points[index], this._points[index - 1], this._points[index + 1]);
+        this._points.splice(index, 1);
     }
 
     public push(mesh: Mesh): void {
@@ -85,6 +76,16 @@ export class TrackGeneratorPointsHandler {
         if (this._points[0]) {
             this._points[0].material = C.START_POINT_MATERIAL;
         }
+    }
+
+    private toVector2(v: Vector3): Vector2 {
+        return new Vector2(v.x, v.z);
+    }
+
+    private closeLoop(): void {
+        this.push(this.trackGen.createDot(
+            this.trackGen.getClientPosition(this._points[0].position),
+            this.top.position));
     }
 
 }
