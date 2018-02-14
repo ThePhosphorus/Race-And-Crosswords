7f8099494375e7a1878f46cf033adca2c59dd944
Randy Sab-Roy <randy.sab.roy@gmail.com>
2018-02-14 14:45:14 -0500

    Imporve intersection validation

--- client/src/app/track-generator-service/constraint-validator/constraint-validator.ts
+++ client/src/app/track-generator-service/constraint-validator/constraint-validator.ts
@@ -12,7 +12,7 @@ export class ConstraintValidator {
     }
 
     public set points(points: Array<Vector3>) {
-      this._points = points;
+        this._points = points;
     }
 
     public validateLine(p1: Vector3, p2: Vector3): boolean {
@@ -21,19 +21,19 @@ export class ConstraintValidator {
         const nextLine: Vector3 = this.findNextLine(p2);
 
         return this.validateLength(line)
-        && this.validateAngle(previousLine, line)
-        && this.validateAngle(line, nextLine)
-        && this.validateIntersection(p1, p2);
+            && this.validateAngle(previousLine, line)
+            && this.validateAngle(line, nextLine)
+            && this.validateIntersection(p1, p2);
     }
 
     private findNextLine(point: Vector3): Vector3 {
         const index: number = this._points.findIndex((p) => p.equals(point));
         if (index === null) {
-        return null;
+            return null;
         }
 
         const nextIndex: number = (index !== this._points.length - 1) ? index + 1 :
-        (this._points[this._points.length - 1].equals(this._points[0])) ? 0 : null;
+            (this._points[this._points.length - 1].equals(this._points[0])) ? 0 : null;
 
         return this._points[nextIndex] ? new Vector3().subVectors(this._points[nextIndex], point) : null;
     }
@@ -41,7 +41,7 @@ export class ConstraintValidator {
     private findPreviousLine(point: Vector3): Vector3 {
         const index: number = this._points.findIndex((p) => p.equals(point));
         const previousIndex: number = (index !== 0) ? index - 1 :
-        (this._points[this._points.length - 1].equals(this._points[0])) ?  this._points.length - LAST_POINT_INDEX : null;
+            (this._points[this._points.length - 1].equals(this._points[0])) ? this._points.length - LAST_POINT_INDEX : null;
 
         return this._points[previousIndex] ? new Vector3().subVectors(point, this._points[previousIndex]) : null;
     }
@@ -54,27 +54,25 @@ export class ConstraintValidator {
         return (v1 == null || v2 == null) || v1.angleTo(v2) < MINIMUM_ANGLE;
     }
 
-    private validateIntersection(p1: Vector3, p2: Vector3): boolean {
-        const line: Vector3 = new Vector3().subVectors(p2, p1);
-        for (let i: number = 0; i < this._points.length - 1; i++) {
-        if (this._points[i + 1] != null && !this._points[i].equals(p1) && !this._points[i + 1].equals(p2)) {
-            const compare: Vector3 = new Vector3().subVectors(this._points[i + 1], this._points[i]);
-            const denominator: number = (compare.z * line.x) - (compare.x * line.z);
-            if (denominator === 0) {
-            return false;
-            }
-            let a: number = p1.z - this._points[i].z;
-            let b: number = p1.x - this._points[i].x;
-            const numerator1: number = ((this._points[i + 1].x - this._points[i].x) * a) - ((this._points[i + 1].z - this._points[i].z) * b);
-            const numerator2: number = ((p2.x - p1.x) * a)
-                                    - ((p2.z - p1.z) * b);
-            a = numerator1 / denominator;
-            b = numerator2 / denominator;
+    private isCounterClockWise(p1: Vector3, p2: Vector3, p3: Vector3): boolean {
+        return (p3.z - p1.z) * (p2.x - p1.x) > (p2.z - p1.z) * (p3.x - p1.x);
+    }
 
-            if (a > 0 && a < 1 && b > 0 && b < 1) {
-            return false;
+    private areDifferentPoints(p1: Vector3, p2: Vector3, p3: Vector3, p4: Vector3): boolean {
+        return !(p1.equals(p2) || p1.equals(p3) || p1.equals(p4) || p2.equals(p3) || p2.equals(p4) || p3.equals(p4));
+    }
+
+    private validateIntersection(p1: Vector3, p2: Vector3): boolean {
+        for (let i: number = 0; i < this._points.length - 1; i++) {
+            const p3: Vector3 = this._points[i];
+            const p4: Vector3 = this._points[i + 1];
+            if (this.areDifferentPoints(p1, p2, p3, p4)) {
+                const doesIntersect: boolean = this.isCounterClockWise(p1, p3, p4) !== this.isCounterClockWise(p2, p3, p4)
+                                            && this.isCounterClockWise(p1, p2, p3) !== this.isCounterClockWise(p1, p2, p4);
+                if (doesIntersect) {
+                    return false;
+                }
             }
-        }
         }
 
         return true;
