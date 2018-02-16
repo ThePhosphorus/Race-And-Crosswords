ee135010a3915b6adc0e82c3f1aa0b95256a10b7
Immaybemarc <immaybemarc@hotmail.com>
2018-02-16 11:53:04 -0500

    Create abstract sound container and engine sound

---
+++ client/src/app/sound-manager-service/sound-containers/abstract-sound-container.ts
@@ -0,0 +1,36 @@
+
+import {
+    PositionalAudio,
+    AudioLoader,
+    AudioBuffer,
+    Object3D,
+    AudioListener
+} from "three";
+
+const DEFAULT_SOUND_PATH: string = "../../assets/sounds/";
+export const DEFAULT_VOLUME: number = 0.5;
+
+export abstract class AbstractSoundContainer {
+
+    protected sound: PositionalAudio;
+
+    public constructor(soundEmittingObject: Object3D, soundListener: AudioListener, sourcePath?: string) {
+        this.sound = new PositionalAudio(soundListener);
+        let soundPath: string = sourcePath ? sourcePath : DEFAULT_SOUND_PATH;
+        soundPath += this.getFileName();
+        this.loadSound(soundPath)  ;
+        soundEmittingObject.add(this.sound);
+    }
+
+    protected loadSound(path: string): void {
+        const idleLoader: AudioLoader = new AudioLoader();
+        idleLoader.load(path, (buffer: AudioBuffer) => {
+            this.setSoundSettings(buffer);
+            },          () => { } , () => { });
+
+    }
+
+    protected abstract setSoundSettings(buffer: AudioBuffer): void;
+
+    protected abstract getFileName(): string;
+}
---
+++ client/src/app/sound-manager-service/sound-containers/engine-sound.ts
@@ -0,0 +1,25 @@
+import {AbstractSoundContainer, DEFAULT_VOLUME} from "./abstract-sound-container";
+import { AudioBuffer} from "three";
+
+const FILE_NAME: string = "engine.ogg";
+const MAX_RPM: number = 5500;
+const MIN_RPM: number = 800;
+export class EngineSound extends AbstractSoundContainer {
+
+    protected setSoundSettings(buffer: AudioBuffer): void {
+        this.sound.setBuffer(buffer);
+        this.sound.setLoop(true);
+        this.sound.setVolume(DEFAULT_VOLUME);
+    }
+    protected getFileName(): string {
+        return FILE_NAME;
+    }
+
+    public updateRPM(rpm: number): void {
+        this.sound.setPlaybackRate(this.getPlaybackRate(rpm));
+    }
+
+    private getPlaybackRate(rpm: number): number {
+        return (rpm - MIN_RPM) / (MAX_RPM - MIN_RPM) + 1; // Augmentation lineaire de la vitesse de lecture (entre 1 et 2)
+    }
+}
