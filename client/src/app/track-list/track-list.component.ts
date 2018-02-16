783258650fa38ee016c4db519ca7aa6c18fd456b
Randy Sab-Roy <randy.sab.roy@gmail.com>
2018-02-16 09:12:12 -0500

    Use a loop to display tracks

--- client/src/app/track-list/track-list.component.ts
+++ client/src/app/track-list/track-list.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
+import { Track } from "./track";
+
+const FAKE_TRACKS_NUMBER: number = 4;
 
 @Component({
-  selector: "app-track-list",
-  templateUrl: "./track-list.component.html",
-  styleUrls: ["./track-list.component.css"]
+    selector: "app-track-list",
+    templateUrl: "./track-list.component.html",
+    styleUrls: ["./track-list.component.css"]
 })
-export class TrackListComponent implements OnInit {
+export class TrackListComponent {
+    public tracks: Array<Track>;
 
-  public constructor() { }
+    public constructor() {
+        this.tracks = new Array<Track>();
 
-  public ngOnInit() {
-  }
+        for (let i: number = 0; i < FAKE_TRACKS_NUMBER; i++) {
+            this.tracks.push(new Track(i, "Test track that is super important"));
+        }
+    }
 
 }
