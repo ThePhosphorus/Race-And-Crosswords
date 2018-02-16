2c6b75905d86b46c241719fbf74a39241ec2a4c9
Immaybemarc <immaybemarc@hotmail.com>
2018-02-16 10:20:45 -0500

    sound work (with Thierry)

--- client/src/app/sound-manager-service/sound-manager.service.ts
+++ client/src/app/sound-manager-service/sound-manager.service.ts
@@ -15,23 +15,21 @@ export class SoundManagerService {
         this.init();
     }
     private init(): void {
-        const tmpLoop: Audio = this.engineLoop = new Audio(this.cameraManager.listener);
+        this.engineLoop = new Audio(this.cameraManager.listener);
         const idleLoader: AudioLoader = new AudioLoader();
         idleLoader.load("../../assets/sounds/engine.ogg", (buffer: AudioBuffer) => {
-                tmpLoop.setBuffer(buffer);
-                tmpLoop.setLoop(true);
-                tmpLoop.setVolume(0.5);
-            },() => { } ,() => { });
+            this.engineLoop.setBuffer(buffer);
+            this.engineLoop.setLoop(true);
+            this.engineLoop.setVolume(0.5);
+            },          () => { } , () => { });
 
     }
 
     public modifyPlayBackSpeed(rpm: number): void {
         if (rpm > 800) {
-            this.engineLoop.stop();
-            this.engineLoop.playbackRate = (rpm - 800) / 4700 + 1;
-            this.engineLoop.play();
-        } else {
-            this.engineLoop.playbackRate = 1;
+            // this.engineLoop.stop();
+            this.engineLoop.setPlaybackRate((rpm - 800) / 4700 + 1);
+           // this.engineLoop.play();
         }
     }
     public startSounds(): void {
