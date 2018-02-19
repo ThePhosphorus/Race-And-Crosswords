bbeef2d08a84787227a3e94c41acbe6256b00dda
Adem Aber Aouni <ademaberaouni@gmail.com>
2018-02-19 08:40:52 -0500

    Added LoadAll from track-loader

--- client/src/app/track-loader/track-loader.service.ts
+++ client/src/app/track-loader/track-loader.service.ts
@@ -1,10 +1,18 @@
 import { Injectable } from "@angular/core";
 import { Vector3 } from "three";
-import { Vector3Struct } from "../../../../common/communication/track";
+import { Vector3Struct, Track } from "../../../../common/communication/track";
+import { HttpClient } from "@angular/common/http";
+
+const BACKEND_URL: string = "http://localhost:3000/";
+const TRACK_SAVER_URL: string = BACKEND_URL + "saver/";
 
 @Injectable()
 export class TrackLoaderService {
-    public constructor() {}
+    public constructor(private http: HttpClient) {}
+
+    public async loadAll(): Promise<Array<Track>> {
+        return this.http.get(TRACK_SAVER_URL + "all").toPromise().then((tracks: Track[]) => tracks);
+    }
 
     public toVector(vector: Vector3Struct): Vector3 {
         return new Vector3(vector.x, vector.y, vector.z);
