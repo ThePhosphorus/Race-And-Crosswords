10d4877b17b1933994f9b953cd0b627353f4453d
Adem Aber Aouni <ademaberaouni@gmail.com>
2018-02-18 21:48:15 -0500

    Added Put And getAll fonctions that works well

--- server/app/race/trackSaver/trackSaver.ts
+++ server/app/race/trackSaver/trackSaver.ts
@@ -3,7 +3,7 @@ import { injectable, inject } from "inversify";
 import { WebService } from "../../webServices";
 import { Track } from "../../../../common/communication/track";
 import { DbClient } from "../../mongo/DbClient";
-import { Collection, InsertOneWriteOpResult } from "mongodb";
+import { Collection, InsertOneWriteOpResult, ReplaceWriteOpResult, ObjectId } from "mongodb";
 import Types from "../../types";
 
 const TRACK_COLLECTION: string = "tracks";
@@ -33,13 +33,34 @@ export class TrackSaver extends WebService {
             const track: Track = req.body["track"];
             this.postTrack(track).then( (result: InsertOneWriteOpResult) => res.send(result));
         });
+
+        this._router.put("/:id", (req: Request, res: Response, next: NextFunction) => {
+            const id: string = req.params.id;
+            const track: Track = req.body["track"];
+            this.putTrack(id, track).then((result: ReplaceWriteOpResult) => res.send(result));
+        });
+
+        this._router.get("/all", (req: Request, res: Response, next: NextFunction) => {
+            this.getAllTracks().then((tracks: Track[]) => res.send(tracks));
+        });
     }
 
     private postTrack(track: Track): Promise<InsertOneWriteOpResult> {
         this.connect();
+        track._id = undefined;
 
         return this.collection.insertOne(track);
     }
 
-    private
+    private putTrack(id: string, track: Track): Promise<ReplaceWriteOpResult> {
+        this.connect();
+
+        return this.collection.replaceOne({_id : new ObjectId(id)}, track);
+    }
+
+    private getAllTracks(): Promise<Track[]> {
+        this.connect();
+
+        return this.collection.find({}).toArray();
+    }
 }
