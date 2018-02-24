ff767e99362146067d7e074a56a23762a6a6f52d
Marc Anhoury <immaybemarc@hotmail.com>
2018-02-24 15:27:10 -0500

    Fixed grid generator tests

--- server/app/crosswords/grid/grid-generator.spec.ts
+++ server/app/crosswords/grid/grid-generator.spec.ts
@@ -4,18 +4,21 @@ import { GridGenerator } from "./grid-generator";
 
 const gridGenerator: GridGenerator = new GridGenerator();
 const gridSize: number = 5;
+const TIMEOUT: number = 10000; // Increase test timeout so that grid has time to generate
 
 // tslint:disable-next-line:max-func-body-length
-gridGenerator.getNewGrid(Difficulty.Easy, gridSize).then((grid: CrosswordGrid) => {
-    describe("Grid generation", () => {
-        describe("When the grid is generated", () => {
-            it("should give a grid with the right size", (done: MochaDone) => {
+
+describe("Grid generation", () => {
+    describe("When the grid is generated", () => {
+        it("should give a grid with the right size", (done: MochaDone) => {
+            return gridGenerator.getNewGrid(Difficulty.Easy, gridSize).then((grid: CrosswordGrid) => {
                 assert.strictEqual(grid.size, gridSize, "Attribute size of grid is not the expected size.");
                 assert.strictEqual(grid.grid.length, gridSize * gridSize, "Vertical length is not the right length");
-                done();
             });
+        }).timeout(TIMEOUT);
 
-            it("should give us a grid with no blackTiles in the first row and the first column ", (done: MochaDone) => {
+        it("should give us a grid with no blackTiles in the f irst row and the first column ", (done: MochaDone) => {
+            return gridGenerator.getNewGrid(Difficulty.Easy, gridSize).then((grid: CrosswordGrid) => {
                 for (let i: number = 0; i < gridSize; i++) {
                     if (grid.grid[i].isBlackTile) {
                         assert.fail("Detected blackTile on the first row");
@@ -23,10 +26,11 @@ gridGenerator.getNewGrid(Difficulty.Easy, gridSize).then((grid: CrosswordGrid) =
                         assert.fail("Detected blackTile on the first column");
                     }
                 }
-                done();
             });
+        }).timeout(TIMEOUT);
 
-            it("should have a word on each colomn/row", (done: MochaDone) => {
+        it("should have a word on each colomn/row", (done: MochaDone) => {
+            return gridGenerator.getNewGrid(Difficulty.Easy, gridSize).then((grid: CrosswordGrid) => {
                 const hasAcrossWord: boolean[] = new Array<boolean>(gridSize).fill(false, 0, gridSize - 1);
                 const hasDownWord: boolean[] = new Array<boolean>(gridSize).fill(false, 0, gridSize - 1);
                 for (const word of grid.words) {
@@ -40,36 +44,40 @@ gridGenerator.getNewGrid(Difficulty.Easy, gridSize).then((grid: CrosswordGrid) =
                     assert.ok(hasAcrossWord[i], "There is no word on row : " + i);
                     assert.ok(hasDownWord[i], "There is no word on column : " + i);
                 }
-                done();
             });
+        }).timeout(TIMEOUT);
 
-            it("should give us words with definitions ", (done: MochaDone) => {
+        it("should give us words with definitions ", (done: MochaDone) => {
+            return gridGenerator.getNewGrid(Difficulty.Easy, gridSize).then((grid: CrosswordGrid) => {
                 grid.words.forEach((word: Word) => {
-                    assert.ok(word.definitions.length > 0, " Word : " + word.id );
+                    assert.ok(word.definitions.length > 0, " Word : " + word.id);
                 });
-                done();
             });
+        }).timeout(TIMEOUT);
 
-            it("should not have apostrophe or apostrophe", (done: MochaDone) => {
+        it("should not have apostrophe or apostrophe", (done: MochaDone) => {
+            return gridGenerator.getNewGrid(Difficulty.Easy, gridSize).then((grid: CrosswordGrid) => {
                 grid.grid.forEach((tile: Letter) => {
                     if (!tile.isBlackTile) {
                         assert.notEqual(tile.char, "-", "Has hypen");
                         assert.notEqual(tile.char, "'", "Has apostrophe");
                     }
                 });
-                done();
             });
+        }).timeout(TIMEOUT);
 
-            it("should not have special letters (accents, etc)", (done: MochaDone) => {
+        it("should not have special letters (accents, etc)", (done: MochaDone) => {
+            return gridGenerator.getNewGrid(Difficulty.Easy, gridSize).then((grid: CrosswordGrid) => {
                 grid.grid.forEach((tile: Letter) => {
                     if (!tile.isBlackTile) {
-                        assert.ok(tile.char.normalize("NFD").length === 1 , "Has character : " + tile.char);
+                        assert.ok(tile.char.normalize("NFD").length === 1, "Has character : " + tile.char);
                     }
                 });
-                done();
             });
+        }).timeout(TIMEOUT);
 
-            it("should not have duplicate word", (done: MochaDone) => {
+        it("should not have duplicate word", (done: MochaDone) => {
+            return gridGenerator.getNewGrid(Difficulty.Easy, gridSize).then((grid: CrosswordGrid) => {
                 const words: string[] = new Array<string>();
                 grid.words.forEach((gridWord: Word) => {
                     let wordString: string = "";
@@ -79,10 +87,8 @@ gridGenerator.getNewGrid(Difficulty.Easy, gridSize).then((grid: CrosswordGrid) =
                     assert.equal(words.indexOf(wordString), -1, "Duplicate of : " + wordString);
                     words.push(wordString);
                 });
-                done();
             });
-        });
+        }).timeout(TIMEOUT);
+
     });
-}).catch((err: Error) => {
-    console.error(" Grid generator : " + err.message);
 });
