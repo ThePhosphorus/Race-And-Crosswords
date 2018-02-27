1ad228b61f228950c7aaf23d88eff4f83b425245
Adem Aber Aouni <ademaberaouni@gmail.com>
2018-02-26 19:57:29 -0500

    Removed useless ngOnInit

--- client/src/app/home-page/home-page.component.ts
+++ client/src/app/home-page/home-page.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 
 @Component({
   selector: "app-home-page",
   templateUrl: "./home-page.component.html",
   styleUrls: ["./home-page.component.css"]
 })
-export class HomePageComponent implements OnInit {
+export class HomePageComponent {
 
   public constructor() { }
-
-  public ngOnInit(): void {
-  }
 
 }
