04595e7f25b6fbe58e3065129f067c524583264a
Randy Sab-Roy <randy.sab.roy@gmail.com>
2018-03-05 16:45:03 -0500

    Allow to specify a width to the track

--- client/src/app/race/track-loader/track-loader.service.ts
+++ client/src/app/race/track-loader/track-loader.service.ts
@@ -5,7 +5,7 @@ import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs/Observable";
 import "rxjs/add/operator/map";
 import { BACKEND_URL, HALF, PI_OVER_2 } from "../../global-constants/constants";
-import { TRACK_WIDTH } from "../race.constants";
+import { DEFAULT_TRACK_WIDTH } from "../race.constants";
 
 const TRACK_PATH: string = "../../assets/textures/floor.jpg";
 const TRACK_SAVER_URL: string = BACKEND_URL + "race/saver/";
@@ -30,22 +30,23 @@ export class TrackLoaderService {
         return meshs;
     }
 
-    public static getTrackMaterial(length: number): MeshLambertMaterial {
+    public static getTrackMaterial(width: number, length: number): MeshLambertMaterial {
         const texture: Texture = new TextureLoader().load(TRACK_PATH);
         texture.wrapS = RepeatWrapping;
         texture.wrapT = RepeatWrapping;
-        texture.repeat.set(TRACK_WIDTH * FLOOR_RATIO, length * FLOOR_RATIO);
+        texture.repeat.set(width * FLOOR_RATIO, length * FLOOR_RATIO);
 
         return new MeshLambertMaterial({ map: texture, side: DoubleSide });
     }
 
-    public static getRoad(pointA: Vector3, pointB: Vector3): Mesh {
+    public static getRoad(pointA: Vector3, pointB: Vector3, width?: number): Mesh {
+        const trackWidth: number = width ? width : DEFAULT_TRACK_WIDTH;
         const vecAB: Vector3 = pointB.clone().sub(pointA);
-        const distanceAB: number =  vecAB.length() + TRACK_WIDTH;
-        const geo: PlaneGeometry = new PlaneGeometry(TRACK_WIDTH, distanceAB);
+        const distanceAB: number =  vecAB.length() + DEFAULT_TRACK_WIDTH;
+        const geo: PlaneGeometry = new PlaneGeometry(trackWidth, distanceAB);
         geo.rotateX( - PI_OVER_2);
 
-        const mesh: Mesh = new Mesh(geo, TrackLoaderService.getTrackMaterial(distanceAB));
+        const mesh: Mesh = new Mesh(geo, TrackLoaderService.getTrackMaterial(trackWidth, distanceAB));
         const positionOfTheRoad: Vector3 = pointA.clone().add(vecAB.clone().multiplyScalar(HALF));
 
         mesh.position.copy(positionOfTheRoad);
