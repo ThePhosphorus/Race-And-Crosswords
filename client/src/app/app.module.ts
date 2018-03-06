20ae9aa5d24547f98841de6873d1cf0122efec32
Elisabeth <elisabeth.lafleur.mtl@gmail.com>
2018-03-06 14:29:21 -0500

    Generating HUD component
    w/ Thierry

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -16,6 +16,7 @@ import { CrosswordGameInfoComponent } from "./crossword/crossword-game-info/cros
 import { InputGridComponent } from "./crossword/input-grid/input-grid.component";
 import { DefinitionComponent } from "./crossword/definition/definition.component";
 import { InputLetterComponent } from "./crossword/input-letter/input-letter.component";
+import { HudComponent } from './hud/hud.component';
 
 @NgModule({
     declarations: [
@@ -31,7 +32,8 @@ import { InputLetterComponent } from "./crossword/input-letter/input-letter.comp
         HomePageComponent,
         InputGridComponent,
         DefinitionComponent,
-        InputLetterComponent
+        InputLetterComponent,
+        HudComponent
     ],
     imports: [
         BrowserModule,
---
+++ client/src/app/hud/hud.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hud',
+  templateUrl: './hud.component.html',
+  styleUrls: ['./hud.component.css']
+})
+export class HudComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
