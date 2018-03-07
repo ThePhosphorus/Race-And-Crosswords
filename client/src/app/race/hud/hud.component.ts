472215b0ed39c9c91a56a7983dd874604b78926c
Unknown <thierry.dubois@polymtl.ca>
2018-03-06 22:41:22 -0500

    Integrated the manager service to access carinfos

--- client/src/app/race/hud/hud.component.ts
+++ client/src/app/race/hud/hud.component.ts
@@ -2,11 +2,13 @@ import { Component, OnInit } from "@angular/core";
 import { TimerObservable } from "rxjs/observable/TimerObservable";
 import { Subscription } from "rxjs/Subscription";
 import { Timer } from "./timer";
+import { GameManagerService, CarInfos } from "../game-component/game-manager-service/game_manager.service";
 
 @Component({
     selector: "app-hud",
     templateUrl: "./hud.component.html",
-    styleUrls: ["./hud.component.css"]
+    styleUrls: ["./hud.component.css"],
+    providers: [GameManagerService]
 })
 export class HudComponent implements OnInit {
     public tick: number;
@@ -14,7 +16,7 @@ export class HudComponent implements OnInit {
     private lapTimer: Timer;
 
     private subscription: Subscription;
-    public constructor() {
+    public constructor(private gameManagerService: GameManagerService) {
         this.initTick();
         this.globalTimer = new Timer();
         this.lapTimer = new Timer();
@@ -25,9 +27,12 @@ export class HudComponent implements OnInit {
         this.tick = 0;
     }
 
-
     public ngOnInit(): void {
         this.start_chron();
+    }
+
+    public get carInfos(): CarInfos {
+        return this.gameManagerService.carInfos;
     }
 
     private start_chron(): void {
