726cf3053acc05a59d4d353ede5361a4d687b4ec
Unknown <thierry.dubois@polymtl.ca>
2018-03-07 18:40:04 -0500

    Fixed an error

--- client/src/app/race/hud/rpm-bar/rpm-bar.component.ts
+++ client/src/app/race/hud/rpm-bar/rpm-bar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { GameManagerService, CarInfos } from '../../game-component/game-manager-service/game_manager.service';
+import { GameManagerService } from '../../game-component/game-manager-service/game_manager.service';
 
 @Component({
   selector: 'app-rpm-bar',
