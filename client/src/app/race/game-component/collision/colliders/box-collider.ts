0de248af270971d58c739d47ef2334d5fe2d872a
Randy Sab-Roy <randy.sab.roy@gmail.com>
2018-03-13 09:26:56 -0400

    Separating axis theorem

--- client/src/app/race/game-component/collision/colliders/box-collider.ts
+++ client/src/app/race/game-component/collision/colliders/box-collider.ts
@@ -1,5 +1,5 @@
 import { Collider } from "./collider";
-import { Vector3 } from "three";
+import { Vector3, Vector2, Mesh, PlaneGeometry, MeshBasicMaterial, DoubleSide } from "three";
 
 const HALF: number = 0.5;
 
@@ -15,6 +15,7 @@ export class BoxCollider extends Collider {
         this.height = height;
         this.offset = offset.clone();
         this.radius = this.pythagore(this.width * HALF, this.height * HALF);
+        this.add(new Mesh( new PlaneGeometry( this.width, this.height).rotateX(-Math.PI/2).translate(0,2,0), new MeshBasicMaterial( {color: 0xffff00, side: DoubleSide} ) ));
     }
 
     public getBroadRadius(): number {
@@ -23,6 +24,33 @@ export class BoxCollider extends Collider {
 
     public getAbsolutePosition(): Vector3 {
         return this.parent.position.clone().add(this.offset);
+    }
+
+    public getNormals(): Array<Vector2> {
+        const normals: Array<Vector2> = new Array<Vector2>();
+        const vertexes: Array<Vector2> = this.getVertexes();
+        for (const vertex of vertexes) {
+            const normal: Vector2 = new Vector2(
+                vertex.y,
+                vertex.x
+            );
+            normals.push(normal);
+        }
+
+        return normals;
+    }
+
+    public getVertexes(): Array<Vector2> {
+        const vertexes: Array<Vector2> = new Array<Vector2>();
+        const absPos: Vector3 = this.getAbsolutePosition();
+        const x: number = absPos.x;
+        const y: number = absPos.z;
+        vertexes.push(new Vector2(x - HALF * this.width, y - HALF * this.height));
+        vertexes.push(new Vector2(x - HALF * this.width, y + HALF * this.height));
+        vertexes.push(new Vector2(x + HALF * this.width, y - HALF * this.height));
+        vertexes.push(new Vector2(x + HALF * this.width, y + HALF * this.height));
+
+        return vertexes;
     }
 
     private pythagore(x: number, y: number): number {
--- client/src/app/race/game-component/collision/collision-detector.service.ts
+++ client/src/app/race/game-component/collision/collision-detector.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from "@angular/core";
-import { Scene } from "three";
+import { Scene, Vector2 } from "three";
 import { Collider } from "./colliders/collider";
 import { BoxCollider } from "./colliders/box-collider";
 import { LineCollider } from "./colliders/line-collider";
+
+class MinMax {
+    public minProj: number;
+    public maxProj: number;
+    public minIndex: number;
+    public maxIndex: number;
+}
 
 @Injectable()
 export class CollisionDetectorService {
@@ -35,7 +42,7 @@ export class CollisionDetectorService {
     }
 
     private broadDetection(coll1: Collider, coll2: Collider): boolean {
-        return !(coll1 instanceof LineCollider && coll2 instanceof LineCollider) ||
+        return !(coll1 instanceof LineCollider && coll2 instanceof LineCollider) &&
             coll2.getAbsolutePosition().clone().sub(
                 coll1.getAbsolutePosition()).length() <= (coll1.getBroadRadius() + coll2.getBroadRadius());
     }
@@ -53,7 +60,50 @@ export class CollisionDetectorService {
     }
 
     private boxBoxDetection(coll1: BoxCollider, coll2: BoxCollider): boolean {
+        const normals1: Array<Vector2> = coll1.getNormals();
+        const normals2: Array<Vector2> = coll2.getNormals();
+        const vertexes1: Array<Vector2> = coll1.getVertexes();
+        const vertexes2: Array<Vector2> = coll2.getVertexes();
+
+        for (const normal of normals1) {
+            const minMax1: MinMax = this.getMinMax(vertexes1, normal);
+            const minMax2: MinMax = this.getMinMax(vertexes2, normal);
+            if (minMax1.maxProj < minMax2.minProj || minMax2.maxProj < minMax1.minProj) {
+                return false;
+            }
+        }
+
+        for (const normal of normals2) {
+            const minMax1: MinMax = this.getMinMax(vertexes1, normal);
+            const minMax2: MinMax = this.getMinMax(vertexes2, normal);
+            if (minMax1.maxProj < minMax2.minProj || minMax2.maxProj < minMax1.minProj) {
+                return false;
+            }
+        }
+
         return true;
+    }
+
+    private getMinMax(vertexes: Array<Vector2>, axis: Vector2): MinMax {
+        const minMax: MinMax = new MinMax();
+        minMax.minProj = vertexes[0].dot(axis);
+        minMax.maxProj = vertexes[0].dot(axis);
+        minMax.minIndex = 0;
+        minMax.maxIndex = 0;
+
+        for (let i: number = 1; i < vertexes.length; i++) {
+            const currProj: number = vertexes[i].dot(axis);
+            if (minMax.minProj > currProj) {
+                minMax.minProj = currProj;
+                minMax.minIndex = i;
+            }
+            if (currProj > minMax.maxProj) {
+                minMax.maxProj = currProj;
+                minMax.maxIndex = i;
+            }
+        }
+
+        return minMax;
     }
 
     private boxLineDetection(box: BoxCollider, line: LineCollider): boolean {
