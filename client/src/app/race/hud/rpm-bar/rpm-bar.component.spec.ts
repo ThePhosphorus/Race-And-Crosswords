5235fb32cebbc310127954ca4c41013fc0b0b241
Unknown <thierry.dubois@polymtl.ca>
2018-03-16 17:07:41 -0400

    Modified test for tslint

--- client/src/app/race/hud/rpm-bar/rpm-bar.component.spec.ts
+++ client/src/app/race/hud/rpm-bar/rpm-bar.component.spec.ts
@@ -1,8 +1,8 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { RpmBarComponent } from './rpm-bar.component';
+import { RpmBarComponent } from "./rpm-bar.component";
 
-describe('RpmBarComponent', () => {
+describe("RpmBarComponent", () => {
   let component: RpmBarComponent;
   let fixture: ComponentFixture<RpmBarComponent>;
 
@@ -19,7 +19,7 @@ describe('RpmBarComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
