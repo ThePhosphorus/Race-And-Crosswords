41e002320b63cf22f2b88144ddb1d5fdf8539075
Marc Anhoury <immaybemarc@hotmail.com>
2018-03-18 12:11:52 -0400

    Added min vertex array projection

--- client/src/app/race/game-component/collision/projection.ts
+++ client/src/app/race/game-component/collision/projection.ts
@@ -3,6 +3,8 @@ import { Vector2 } from "three";
 export class Projected {
     private _minProjected: number;
     private _maxProjected: number;
+    private _minVertexes: Array<Vector2>;
+    private _maxVertexes: Array<Vector2>;
 
     public constructor(objectToProject: Array<Vector2>, projectionAxis: Vector2) {
         this.project(objectToProject, projectionAxis);
@@ -14,6 +16,14 @@ export class Projected {
 
     public get maxProjected(): number {
         return this._maxProjected;
+    }
+
+    public get maxVertexes(): Array<Vector2> {
+        return this._maxVertexes;
+    }
+
+    public get minVertexes(): Array<Vector2> {
+        return this._minVertexes;
     }
 
     public distance(projected: Projected): number {
@@ -28,8 +38,18 @@ export class Projected {
             const currProj: number = vertex.dot(axis);
             if (this._minProjected > currProj) {
                 this._minProjected = currProj;
-            } else if (currProj > this._maxProjected) {
+                this._minVertexes = new Array<Vector2>();
+                this._minVertexes.push(vertex);
+            } else if (currProj === this._minProjected) {
+                this._minVertexes.push(vertex);
+            }
+
+            if (currProj > this._maxProjected) {
                 this._maxProjected = currProj;
+                this._maxVertexes = new Array<Vector2>();
+                this._maxVertexes.push(vertex);
+            } else if (currProj === this._maxProjected) {
+                this._maxVertexes.push(vertex);
             }
         }
 
