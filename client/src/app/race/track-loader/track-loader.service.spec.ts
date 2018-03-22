e6d44ee5e46edca4b7739512343b77b53599c822
Randy Sab-Roy <randy.sab.roy@gmail.com>
2018-03-22 16:22:31 -0400

    Remove unused import

--- client/src/app/race/track-loader/track-loader.service.spec.ts
+++ client/src/app/race/track-loader/track-loader.service.spec.ts
@@ -1,7 +1,5 @@
 import { TestBed, inject } from "@angular/core/testing";
 import { TrackLoaderService } from "./track-loader.service";
-import { Vector3Struct } from "../../../../../common/communication/track";
-import { Vector3 } from "three";
 import { HttpClientModule } from "@angular/common/http/";
 
 /* tslint:disable:no-magic-numbers */
