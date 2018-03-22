75b128e24cdb20569d089a0915b29fe0eeea645b
Randy Sab-Roy <randy.sab.roy@gmail.com>
2018-03-22 12:14:31 -0400

    Remove point detection

--- client/src/app/race/game-component/collision/collision-detector.service.ts
+++ client/src/app/race/game-component/collision/collision-detector.service.ts
@@ -14,7 +14,6 @@ export class CollisionDetectorService {
     public constructor() { }
 
     public detectCollisions(scene: Scene): void {
-        // Fetch all colliders
         const colliders: Array<Collider> = this.getColliders(scene);
         for (let i: number = 0; i < colliders.length; i++) {
             for (let j: number = i + 1; j < colliders.length; j++) {
@@ -71,47 +70,11 @@ export class CollisionDetectorService {
             if (Math.abs(distance) < minDistance) {
                 minDistance = Math.abs(distance);
                 distanceNormal = normal.clone();
-                // this.addCollisionIntersection(collision, projected1, projected2);
             }
         }
 
-        // collision.overlap = -minDistance;
-        // collision.contactAngle = contact.angle();
-
         return new Collision(coll1, coll2, distanceNormal, -minDistance);
     }
-
-    // private addCollisionIntersection(collision: Collision, projected1: Projected, projected2: Projected): void {
-    //     collision.collidingPoint1 = this.findIntersection(projected1, projected2);
-    //     collision.collidingPoint2 = this.findIntersection(projected2, projected1);
-    // }
-
-    // private findIntersection(projected1: Projected, projected2: Projected): Vector2 {
-    //     const maxProjectedDistance: number = projected2.minProjected - projected1.maxProjected;
-    //     const minProjectedDistance: number = projected1.minProjected - projected2.maxProjected;
-
-    //     const projected1Vertexes: Array<Vector2> =
-    //         maxProjectedDistance > minProjectedDistance ? projected1.maxVertexes : projected1.minVertexes;
-    //     const projected2Vertexes: Array<Vector2> =
-    //         maxProjectedDistance > minProjectedDistance ? projected2.minVertexes : projected2.maxVertexes;
-
-    //     if (projected1Vertexes.length === 1) {
-    //         return projected1Vertexes[0];
-    //     } else {
-    //         if (projected2Vertexes.length === 1) {
-    //             return this.projectPoint(projected2Vertexes[0], projected1Vertexes);
-    //         } else {
-    //             return this.projectPoint(projected2Vertexes[0], projected1Vertexes);
-    //         }
-    //     }
-    // }
-
-    // private projectPoint(point: Vector2, line: Array<Vector2>): Vector2 {
-    //     const axis: Vector2 = line[0].sub(line[1]).normalize();
-    //     const projection: number = point.dot(axis);
-
-    //     return line[0].add(axis.multiplyScalar(projection));
-    // }
 
     private resolveCollision(collision: Collision): void {
         const rb1: RigidBody = collision.coll1.parent.children.find((c) => c instanceof RigidBody) as RigidBody;
@@ -140,7 +103,5 @@ export class CollisionDetectorService {
         const antiOverlap2: Vector2 = collision.normal.clone().multiplyScalar(-OVERLAP_FACTOR * sign * collision.overlap);
         rb1.parent.position.add(new Vector3(antiOverlap1.x, 0, antiOverlap1.y));
         rb2.parent.position.add(new Vector3(antiOverlap2.x, 0, antiOverlap2.y));
-
-        console.log(antiOverlap1);
     }
 }
--- client/src/app/race/game-component/collision/collision.ts
+++ client/src/app/race/game-component/collision/collision.ts
@@ -4,16 +4,12 @@ import { Vector2 } from "three";
 export class Collision {
     public coll1: Collider;
     public coll2: Collider;
-    // public collidingPoint1: Vector2;
-    // public collidingPoint2: Vector2;
     public overlap: number;
     public normal: Vector2;
 
     public constructor(coll1: Collider, coll2: Collider, normal: Vector2, overlap: number) {
         this.coll1 = coll1;
         this.coll2 = coll2;
-        // this.collidingPoint1 = collidingPoint1;
-        // this.collidingPoint2 = collidingPoint2;
         this.normal = normal;
         this.overlap = overlap;
     }
