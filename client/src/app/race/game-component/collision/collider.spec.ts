df346d9815db68bad5d6059af30395bfc259b9e0
Marc Anhoury <immaybemarc@hotmail.com>
2018-03-22 15:13:42 -0400

    added collider tests

--- client/src/app/race/game-component/collision/collider.spec.ts
+++ client/src/app/race/game-component/collision/collider.spec.ts
@@ -1,11 +1,30 @@
 import { Collider } from "./collider";
+import { Object3D, Vector2 } from "three";
 
 const COLLIDER_WIDTH: number = 5;
 const COLLIDER_LENGTH: number = 5;
 
-describe("CollisionDetectorService", () => {
+describe("Collisider", () => {
     it("should be created", () => {
         const collider: Collider = new Collider(COLLIDER_WIDTH, COLLIDER_LENGTH);
         expect(collider).toBeTruthy();
+    });
+
+    it("should have normals perpendicular to the sides", () => {
+        const collider: Collider = new Collider(COLLIDER_WIDTH, COLLIDER_LENGTH);
+        const object: Object3D = new Object3D();
+        object.add(collider);
+        const colliderVertexes: Vector2[] = collider.getAbsoluteVertexes2D();
+        const colliderNormals: Vector2[] = collider.getAbsoluteVertexes2D();
+
+        for (let i: number = 0; i < colliderNormals.length; i++) {
+            const vertex2: Vector2 = i < colliderVertexes.length - 1 ? colliderVertexes[i + 1] : colliderVertexes[0];
+            const line: Vector2 = colliderVertexes[i].clone().sub(vertex2);
+
+            const dotProduct: number = line.dot(colliderNormals[i]);
+
+            expect(dotProduct).toEqual(0);
+        }
+
     });
 });
