f55a3f1fe765764b15f393ac32d729ec0a668088
Unknown <thierry.dubois@polymtl.ca>
2018-03-22 14:32:36 -0400

    Removed unused code

--- client/src/app/race/hud/hud.component.ts
+++ client/src/app/race/hud/hud.component.ts
@@ -1,10 +1,6 @@
 import { Component, OnInit } from "@angular/core";
-import { TimerObservable } from "rxjs/observable/TimerObservable";
-import { Subscription } from "rxjs/Subscription";
 import { Timer } from "./timer";
 import { GameManagerService, CarInfos } from "../game-component/game-manager-service/game_manager.service";
-import { TWO_SECONDS, CENTISECOND } from "../../global-constants/constants";
-import { Observable } from "rxjs/Observable";
 
 @Component({
     selector: "app-hud",
@@ -36,8 +32,6 @@ export class HudComponent implements OnInit {
     }
 
     private start_chron(): void {
-        // tslint:disable-next-line:no-magic-numbers
-        // tslint:disable-next-line:typedef
         this.gameManagerService.hud.subscribe((t: number) => {
             this.lapTimer.update(t);
             this.globalTimer.update(t);
