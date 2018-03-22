8f93d31040fe4ce42786b4269ee506c0c1fffdfd
Randy Sab-Roy <randy.sab.roy@gmail.com>
2018-03-22 18:43:50 -0400

    Fix dep injection in hud test

--- client/src/app/race/hud/hud.component.spec.ts
+++ client/src/app/race/hud/hud.component.spec.ts
@@ -1,6 +1,12 @@
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { HudComponent } from "./hud.component";
+import { GameManagerService } from "../game-component/game-manager-service/game_manager.service";
+import { CameraManagerService } from "../camera-manager-service/camera-manager.service";
+import { InputManagerService } from "../input-manager-service/input-manager.service";
+import { SoundManagerService } from "../game-component/sound-manager-service/sound-manager.service";
+import { CollisionDetectorService } from "../game-component/collision/collision-detector.service";
+import { LightManagerService } from "../game-component/light-manager/light-manager.service";
 
 describe("HudComponent", () => {
   let component: HudComponent;
@@ -8,7 +14,13 @@ describe("HudComponent", () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HudComponent ]
+      declarations: [ HudComponent ],
+      providers: [GameManagerService,
+                  CameraManagerService,
+                  InputManagerService,
+                  SoundManagerService,
+                  CollisionDetectorService,
+                  LightManagerService]
     })
     .compileComponents();
   }));
