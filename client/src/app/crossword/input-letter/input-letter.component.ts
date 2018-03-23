6e044c9fa7c655cf87a847d3ee04cab831540dca
Adem Aber Aouni <ademaberaouni@gmail.com>
2018-03-23 15:23:23 -0400

    Select w/ Elizabeth

--- client/src/app/crossword/input-letter/input-letter.component.ts
+++ client/src/app/crossword/input-letter/input-letter.component.ts
@@ -12,13 +12,11 @@ export class InputLetterComponent implements OnInit {
     @Input() public letter: string; // TODO: Get object from service and not trough @Input() (We already know the id)
     @Input() public id: number;
     private _gridState: GridState;
-    private _isOtherPlayerSelected: boolean;
 
     public constructor(private _crosswordService: CrosswordService) {
         this.letter = "	 ";
         this.id = 1;
         this._gridState = new GridState();
-        this._isOtherPlayerSelected = false;
     }
 
     public ngOnInit(): void {
@@ -39,10 +37,6 @@ export class InputLetterComponent implements OnInit {
         return this._gridState.LIsHovered(this.id);
     }
 
-    public isHighlighted(): boolean {
-        return this.playerHiglightCSS !== {};
-    }
-
     public isCurrentLetter(): boolean {
         return this._gridState.LIsCurrentLetter(this.id);
     }
@@ -52,9 +46,8 @@ export class InputLetterComponent implements OnInit {
         let bgColor: string = "white";
         const players: Array<PlayerId> = this._crosswordService.getLetterHighlightPlayers(this.id);
         if (players.length === 0) {
-
             return {};
-        } else if (players.length === 1) {
+        } else {
             color = this._crosswordService.getPlayerColor(players[0], true);
             bgColor = this._crosswordService.getPlayerColor(players[0], false);
 
@@ -67,21 +60,6 @@ export class InputLetterComponent implements OnInit {
                 "box-shadow": "0vmin 0vmin 0vmin 0.4vmin " + color + ",inset 0vmin 0vmin 1.5vmin " + color,
                 "background-color": bgColor
             };
-        } else {
-            bgColor = this._crosswordService.getPlayerColor(players[1], true);
-            const secondBgColor: string = this._crosswordService.getPlayerColor(players[1], true);
-            const lineGradiant: string = "repeating-linear-gradient(45deg" +
-                        bgColor + "25%," + bgColor + "25%," + secondBgColor + "25%," + secondBgColor + "25%);";
-
-            return {
-                "border-style" : "dotted",
-                "width" : "90%",
-                "height" : "90%",
-                "border-width": "0.2vmin",
-                "border-color" : lineGradiant,
-                "background-image": lineGradiant
-            };
-        }
 
     }
 }
