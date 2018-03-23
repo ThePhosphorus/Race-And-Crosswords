8f178e1f410e994eea1e35aec8ce32b86be5ed2d
Randy Sab-Roy <randy.sab.roy@gmail.com>
2018-03-23 13:44:15 -0400

    Iterate over walls

--- client/src/app/race/game-component/road/road.ts
+++ client/src/app/race/game-component/road/road.ts
@@ -1,7 +1,12 @@
-import { Mesh, Object3D, PlaneGeometry, Plane } from "three";
+import { Mesh, Object3D, PlaneGeometry, Vector2 } from "three";
 import { Collider } from "../collision/collider";
+import { RigidBody } from "../rigid-body/rigid-body";
 
-const WALL_WIDTH: number = 0.001;
+const WALL_WIDTH: number = 0.1;
+
+class WallDimensions {
+    public constructor(public length: number, public offset: Vector2) {}
+}
 
 export class Road extends Object3D {
 
@@ -13,36 +18,58 @@ export class Road extends Object3D {
     }
 
     private initTrack(): void {
-        // meshs.forEach((m) => this.add(m));
         for (let i: number = 0; i < this.meshs.length; i++) {
             if (this.meshs[i].geometry instanceof PlaneGeometry) {
-                // const prev: Mesh = this.findPrevious(i);
-                // const next: Mesh = this.findNext(i);
-                // const coll1: Collider = new Collider(WALL_WIDTH, 0);
-                this.add(this.meshs[i]);
+                this.meshs[i].geometry.computeBoundingBox();
+                const prev: Mesh = this.findPrevious(i);
+                const next: Mesh = this.findNext(i);
+                const coll1: Collider = new Collider(WALL_WIDTH, this.calculateLeftWallDimension(this.meshs[i], prev, next).length);
+                const coll2: Collider = new Collider(WALL_WIDTH, this.calculateRightWallDimension(this.meshs[i], prev, next).length);
+                const rb: RigidBody = new RigidBody(1, true);
+                this.meshs[i].add(rb, coll1, coll2);
             }
+            this.add(this.meshs[i]);
         }
     }
 
     private findPrevious(index: number): Mesh {
-        while (!(this.meshs[index] instanceof PlaneGeometry)) {
+        do {
             index--;
             if (index < 0) {
                 index = this.meshs.length - 1;
             }
-        }
+        } while (!(this.meshs[index].geometry instanceof PlaneGeometry));
 
         return this.meshs[index];
     }
 
     private findNext(index: number): Mesh {
-        while (!(this.meshs[index] instanceof PlaneGeometry)) {
+        index = index >= this.meshs.length ? 0 : index;
+        do {
             index++;
             if (index >= this.meshs.length) {
                 index = 0;
             }
-        }
+        } while (!(this.meshs[index++].geometry instanceof PlaneGeometry));
 
         return this.meshs[index];
+    }
+
+    private calculateLeftWallDimension(mesh: Mesh, prev: Mesh, next: Mesh): WallDimensions {
+        const length: number = mesh.geometry.boundingBox.getSize().z;
+        if (mesh.getWorldDirection().clone().cross(next.getWorldDirection().clone()).y > 0) {
+            // do stuff
+        }
+
+        return new WallDimensions(length, new Vector2(0, 0));
+    }
+
+    private calculateRightWallDimension(mesh: Mesh, prev: Mesh, next: Mesh): WallDimensions {
+        const length: number = mesh.geometry.boundingBox.getSize().z;
+        if (mesh.getWorldDirection().clone().cross(next.getWorldDirection().clone()).y > 0) {
+            // do stuff
+        }
+
+        return new WallDimensions(length, new Vector2(0, 0));
     }
 }
