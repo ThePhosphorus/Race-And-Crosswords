180bec375b46a6ab77776658d83444718f73b68b
Randy Sab-Roy <randy.sab.roy@gmail.com>
2018-03-24 15:04:36 -0400

    Add tests for wall creation

--- client/src/app/race/track-loader/track-loader.service.spec.ts
+++ client/src/app/race/track-loader/track-loader.service.spec.ts
@@ -2,10 +2,21 @@ import { TestBed, inject } from "@angular/core/testing";
 import { TrackLoaderService } from "./track-loader.service";
 import { HttpClientModule } from "@angular/common/http/";
 import { Track, Vector3Struct } from "../../../../../common/communication/track";
-import { Mesh } from "three";
+import { Mesh, Object3D } from "three";
+import { DEFAULT_TRACK_WIDTH, DEFAULT_WALL_WIDTH } from "../race.constants";
 
 /* tslint:disable:no-magic-numbers */
 describe("Track Loader", () => {
+
+    const track: Array<Vector3Struct> = new Array<Vector3Struct>();
+
+    beforeAll(() => {
+        track.push(new Vector3Struct(0, 0, 0));
+        track.push(new Vector3Struct(1, 0, 0));
+        track.push(new Vector3Struct(1, 0, 1));
+        track.push(new Vector3Struct(0, 0, 1));
+        track.push(new Vector3Struct(0, 0, 0));
+    });
 
     beforeEach(() => {
         TestBed.configureTestingModule({
@@ -18,13 +29,23 @@ describe("Track Loader", () => {
         expect(service).toBeTruthy();
     }));
 
-    it("should be return mesh", inject([TrackLoaderService], (service: TrackLoaderService) => {
-        const points: Array<Vector3Struct> = new Array<Vector3Struct>();
-        points.push(new Vector3Struct(0, 0, 0));
-        points.push(new Vector3Struct(1, 0, 0));
-        points.push(new Vector3Struct(1, 0, 1));
-        points.push(new Vector3Struct(0, 0, 1));
-        const meshs: Array<Mesh> = TrackLoaderService.getTrackMeshs(new Track("", "", "", points));
-        expect(meshs.length).toBeGreaterThan(points.length);
+    it("should create meshs", inject([TrackLoaderService], (service: TrackLoaderService) => {
+        const meshs: Array<Mesh> = TrackLoaderService.getTrackMeshs(new Track("", "", "", track));
+        expect(meshs.length).toBeGreaterThan(track.length);
+    }));
+
+    it("should create walls", inject([TrackLoaderService], (service: TrackLoaderService) => {
+        const walls: Array<Object3D> = TrackLoaderService.getTrackWalls(new Track("", "", "", track));
+        expect(walls.length).toBe((track.length - 1) * 2);
+    }));
+
+    it("should have parallel walls", inject([TrackLoaderService], (service: TrackLoaderService) => {
+        const walls: Array<Object3D> = TrackLoaderService.getTrackWalls(new Track("", "", "", track));
+        expect(walls[0].getWorldDirection().angleTo(walls[1].getWorldDirection()) % Math.PI).toBeCloseTo(0);
+    }));
+
+    it("should create walls with offset of track width", inject([TrackLoaderService], (service: TrackLoaderService) => {
+        const walls: Array<Object3D> = TrackLoaderService.getTrackWalls(new Track("", "", "", track));
+        expect(walls[1].position.clone().sub(walls[0].position).length()).toBe(DEFAULT_TRACK_WIDTH + DEFAULT_WALL_WIDTH);
     }));
 });
