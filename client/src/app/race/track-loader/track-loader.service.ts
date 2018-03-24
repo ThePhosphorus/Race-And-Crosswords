0cf9a891bcf72b88c724afb7ec5ad4a90cc1443d
Randy Sab-Roy <randy.sab.roy@gmail.com>
2018-03-24 11:28:24 -0400

    Use intersection instead of angle approximation

--- client/src/app/race/track-loader/track-loader.service.ts
+++ client/src/app/race/track-loader/track-loader.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@angular/core";
-import { Vector3, Mesh, PlaneGeometry, TextureLoader,
+import {
+    Vector3, Mesh, PlaneGeometry, TextureLoader,
     Texture, RepeatWrapping, DoubleSide, CircleGeometry, MeshPhongMaterial, Object3D
 } from "three";
 import { Vector3Struct, Track } from "../../../../../common/communication/track";
@@ -22,7 +23,7 @@ const WALL_WIDTH: number = 1.5;
 
 @Injectable()
 export class TrackLoaderService {
-    public constructor(private http: HttpClient) {}
+    public constructor(private http: HttpClient) { }
 
     public static toVector(vector: Vector3Struct): Vector3 {
         return new Vector3(vector.x, vector.y, vector.z);
@@ -31,17 +32,17 @@ export class TrackLoaderService {
     public static getTrackMeshs(track: Track): Mesh[] {
         const meshs: Array<Mesh> = new Array<Mesh>();
         const startMesh: Mesh =
-        TrackLoaderService.getStartMesh(
-            TrackLoaderService.toVector(track.points[0]),
-            TrackLoaderService.toVector(track.points[1])
-           );
+            TrackLoaderService.getStartMesh(
+                TrackLoaderService.toVector(track.points[0]),
+                TrackLoaderService.toVector(track.points[1])
+            );
         meshs.push(startMesh);
         for (let i: number = 0; i < track.points.length - 1; i++) {
             const roadMesh: Mesh = TrackLoaderService.getRoad(
                 TrackLoaderService.toVector(track.points[i]),
                 TrackLoaderService.toVector(track.points[i + 1]));
             if (i === 0) {
-                 roadMesh.position.setY(DOUBLE * Y_OFFSET);
+                roadMesh.position.setY(DOUBLE * Y_OFFSET);
             } else if (i % DOUBLE) { roadMesh.position.setY(Y_OFFSET); }
             meshs.push(roadMesh);
 
@@ -76,28 +77,47 @@ export class TrackLoaderService {
         const vecAB: Vector3 = pointB.clone().sub(pointA);
         const vecBN: Vector3 = pointN.clone().sub(pointB);
 
-        const prevTurn: number = -vecPA.clone().cross(vecAB).normalize().y;
-        const nextTurn: number = -vecAB.clone().cross(vecBN).normalize().y;
-
-        const prevOffset: number = -(vecPA.angleTo(vecAB) / PI_OVER_2) * DEFAULT_TRACK_WIDTH * HALF * relativeOffset * prevTurn;
-        const nextOffset: number = -(vecAB.angleTo(vecBN) / PI_OVER_2) * DEFAULT_TRACK_WIDTH * HALF * relativeOffset * nextTurn;
-
-        const perp: Vector3 = new Vector3(vecAB.z, vecAB.y, -vecAB.x).normalize()
+        const perpAB: Vector3 = new Vector3(vecAB.z, vecAB.y, -vecAB.x).normalize()
             .multiplyScalar(((DEFAULT_TRACK_WIDTH * HALF) + (WALL_WIDTH * HALF)) * relativeOffset);
-        const direction: Vector3 = vecAB.clone().normalize();
-        const distanceAB: number =  vecAB.length();
+        const perpPA: Vector3 = new Vector3(vecPA.z, vecPA.y, -vecPA.x).normalize()
+            .multiplyScalar(((DEFAULT_TRACK_WIDTH * HALF) + (WALL_WIDTH * HALF)) * relativeOffset);
+        const perpBN: Vector3 = new Vector3(vecBN.z, vecBN.y, -vecBN.x).normalize()
+            .multiplyScalar(((DEFAULT_TRACK_WIDTH * HALF) + (WALL_WIDTH * HALF)) * relativeOffset);
 
+        const p1: Vector3 = TrackLoaderService.findIntersection(pointP.clone().add(perpPA), pointA.clone().add(perpPA),
+                                                                pointA.clone().add(perpAB), pointB.clone().add(perpAB));
+
+        const p2: Vector3 = TrackLoaderService.findIntersection(pointA.clone().add(perpAB), pointB.clone().add(perpAB),
+                                                                pointB.clone().add(perpBN), pointN.clone().add(perpBN));
+        const vecP1P2: Vector3 = p2.clone().sub(p1);
         const wall: Object3D = new Object3D();
-        wall.add(new Collider(WALL_WIDTH, distanceAB - (nextOffset) - (prevOffset)),
+        wall.add(new Collider(WALL_WIDTH, vecP1P2.length()),
                  new RigidBody(DEFAULT_MASS, true));
 
-        const positionOfTheRoad: Vector3 = pointA.clone().add(vecAB.clone().multiplyScalar(HALF));
-
-        wall.position.copy(positionOfTheRoad.clone().add(perp).add(direction.clone().multiplyScalar(prevOffset * prevTurn))
-                                                              .add(direction.clone().multiplyScalar(-nextOffset * nextTurn)));
-        wall.lookAt(pointB.clone().add(perp));
+        wall.position.copy(p1.clone().add(vecP1P2.clone().multiplyScalar(HALF)));
+        wall.lookAt(p2);
 
         return wall;
+    }
+
+    public static findIntersection(p1: Vector3, p2: Vector3, p3: Vector3, p4: Vector3): Vector3 {
+        if (p2.equals(p3)) {
+            return p2.clone();
+        }
+        const x1: number = p1.x;
+        const y1: number = p1.z;
+        const x2: number = p2.x;
+        const y2: number = p2.z;
+        const x3: number = p3.x;
+        const y3: number = p3.z;
+        const x4: number = p4.x;
+        const y4: number = p4.z;
+        const intersectionX: number = ((x1 * y2 - y1 * x2) * (x3 - x4) - (x1 - x2) * (x3 * y4 - y3 * x4)) /
+                                      ((x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4));
+        const intersectionY: number = ((x1 * y2 - y1 * x2) * (y3 - y4) - (y1 - y2) * (x3 * y4 - y3 * x4)) /
+                                      ((x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4));
+
+        return new Vector3(intersectionX, p1.y, intersectionY);
     }
 
     public static getTrackMaterial(width: number, length: number): MeshPhongMaterial {
@@ -113,7 +133,7 @@ export class TrackLoaderService {
         const texture: Texture = new TextureLoader().load(LINE_PATH);
         texture.wrapS = RepeatWrapping;
         texture.wrapT = RepeatWrapping;
-        texture.repeat.set(width , length / 4);
+        texture.repeat.set(width, length / 4);
         const mat: MeshPhongMaterial = new MeshPhongMaterial({ map: texture, side: DoubleSide });
         mat.transparent = true;
         mat.opacity = 0.2;
@@ -122,9 +142,9 @@ export class TrackLoaderService {
     }
 
     public static getCornerAprox(center: Vector3, before: Vector3, after: Vector3): Mesh {
-        const circleGeo: CircleGeometry =  new CircleGeometry(HALF * DEFAULT_TRACK_WIDTH, CORNER_NB_SEGMENTS);
-        circleGeo.rotateX( - PI_OVER_2);
-        const circleMesh: Mesh = new Mesh( circleGeo, TrackLoaderService.getTrackMaterial(DEFAULT_TRACK_WIDTH, DEFAULT_TRACK_WIDTH));
+        const circleGeo: CircleGeometry = new CircleGeometry(HALF * DEFAULT_TRACK_WIDTH, CORNER_NB_SEGMENTS);
+        circleGeo.rotateX(- PI_OVER_2);
+        const circleMesh: Mesh = new Mesh(circleGeo, TrackLoaderService.getTrackMaterial(DEFAULT_TRACK_WIDTH, DEFAULT_TRACK_WIDTH));
         circleMesh.position.copy(new Vector3(center.x, center.y + TRIPLE * Y_OFFSET, center.z));
         circleMesh.receiveShadow = true;
 
@@ -133,7 +153,7 @@ export class TrackLoaderService {
 
     public static getStartMesh(point: Vector3, pointB: Vector3, width?: number): Mesh {
         const geo: PlaneGeometry = new PlaneGeometry(DEFAULT_TRACK_WIDTH, DEFAULT_TRACK_WIDTH / 4);
-        geo.rotateX( - PI_OVER_2);
+        geo.rotateX(- PI_OVER_2);
 
         const mesh: Mesh = new Mesh(geo, TrackLoaderService.getFinishLineMaterial(DEFAULT_TRACK_WIDTH, DEFAULT_TRACK_WIDTH));
 
@@ -149,9 +169,9 @@ export class TrackLoaderService {
     public static getRoad(pointA: Vector3, pointB: Vector3, width?: number): Mesh {
         const trackWidth: number = width ? width : DEFAULT_TRACK_WIDTH;
         const vecAB: Vector3 = pointB.clone().sub(pointA);
-        const distanceAB: number =  vecAB.length();
+        const distanceAB: number = vecAB.length();
         const geo: PlaneGeometry = new PlaneGeometry(trackWidth, distanceAB);
-        geo.rotateX( - PI_OVER_2);
+        geo.rotateX(- PI_OVER_2);
 
         const mesh: Mesh = new Mesh(geo, TrackLoaderService.getTrackMaterial(trackWidth, distanceAB));
         const positionOfTheRoad: Vector3 = pointA.clone().add(vecAB.clone().multiplyScalar(HALF));
