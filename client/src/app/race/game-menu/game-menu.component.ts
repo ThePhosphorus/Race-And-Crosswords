addaf8d89f7dceb96c5f6dd532b4b2dc5675d79e
Randy Sab-Roy <randy.sab.roy@gmail.com>
2018-03-24 22:10:24 -0400

    Add modal

--- client/src/app/race/game-menu/game-menu.component.ts
+++ client/src/app/race/game-menu/game-menu.component.ts
@@ -11,10 +11,23 @@ import { Track } from "../../../../../common/communication/track";
 export class GameMenuComponent {
 
     private tracks: Array<Track>;
+    public openedTrack: Track;
+    public hasDetailsOpen: boolean;
 
     public constructor(private _trackLoader: TrackLoaderService) {
         this.tracks = new Array<Track>();
         this._trackLoader.loadAll().subscribe((ts: Track[]) => this.tracks = ts);
+        this.hasDetailsOpen = false;
+    }
+
+    public open(track: Track): void {
+        this.openedTrack = track;
+        this.hasDetailsOpen = true;
+    }
+
+    public close(): void {
+        this.openedTrack = null;
+        this.hasDetailsOpen = false;
     }
 
 }
