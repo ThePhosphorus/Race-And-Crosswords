c23aee90b3d1c3123249386dd0a97d8d31c95e98
Randy Sab-Roy <randy.sab.roy@gmail.com>
2018-03-24 22:39:50 -0400

    Display highscores

--- common/communication/track.ts
+++ common/communication/track.ts
@@ -1,15 +1,23 @@
 export class Track {
-    constructor (
+    public constructor (
         public _id: string,
         public name: string,
         public description: string,
         public points: Vector3Struct[],
-        public nbPlayed: number
+        public nbPlayed: number,
+        public highscores?: Array<Highscore>
     ){}
 }
 
+export class Highscore {
+    public constructor (
+        public name: string,
+        public time: number
+    ) {}
+}
+
 export class Vector3Struct {
-    constructor (
+    public constructor (
         public x: number,
         public y: number,
         public z: number
