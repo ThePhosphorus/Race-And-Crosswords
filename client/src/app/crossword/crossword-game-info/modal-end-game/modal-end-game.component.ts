0e941c811daa0df671c7b92f3bbba452981de95d
Marc Anhoury <immaybemarc@hotmail.com>
2018-03-25 01:08:50 -0400

    added button to configure new game

--- client/src/app/crossword/crossword-game-info/modal-end-game/modal-end-game.component.ts
+++ client/src/app/crossword/crossword-game-info/modal-end-game/modal-end-game.component.ts
@@ -8,14 +8,25 @@ import { Component, OnInit, EventEmitter, Output } from "@angular/core";
 
 export class ModalEndGameComponent implements OnInit {
   @Output() public showModal: EventEmitter<boolean>;
+  @Output() public configureNewGame: EventEmitter<void>;
+  public isVictorious: boolean;
+  public isMultiplayer: boolean;
+
   public constructor() {
     this.showModal = new EventEmitter<boolean>();
-   }
+    this.configureNewGame = new EventEmitter<void>();
+    this.isVictorious = true;
+    this.isMultiplayer = false;
+  }
 
   public ngOnInit(): void {
   }
 
   public closeGameOptions(): void {
     this.showModal.emit(false);
+  }
+
+  public startNewGame(): void {
+    this.configureNewGame.emit();
   }
 }
