27287735579115afd9ecafb455324136f6999bde
Adem Aber Aouni <ademaberaouni@gmail.com>
2018-03-26 17:51:05 -0400

    Fixed Server

--- server/app/crosswords/multiplayer/matchManager.ts
+++ server/app/crosswords/multiplayer/matchManager.ts
@@ -1,7 +1,10 @@
 import { Player } from "../../../../common/communication/Player";
 import msg from "../../../../common/communication/socketTypes";
 import { CrosswordGrid } from "../../../../common/crossword/crossword-grid";
-import { Difficulty, Orientation } from "../../../../common/crossword/enums-constants";
+import {
+  Difficulty,
+  Orientation
+} from "../../../../common/crossword/enums-constants";
 import { Word } from "../../../../common/crossword/word";
 import * as Request from "request-promise-native";
 import { GRID_GENERATION_SERVICE_URL } from "../../constants";
@@ -19,7 +22,7 @@ class SPlayer extends Player {
         public socket: Socket
     ) {
         super(id, name, score);
-     }
+  }
 }
 
 export class MatchManager {
@@ -43,6 +46,10 @@ export class MatchManager {
         return this._players.length > 0;
     }
 
+    public get difficulty(): Difficulty {
+        return this._difficulty;
+    }
+
     public addPlayer(socket: Socket): void {
         const id: number = this.generateid();
         this._players.push(new SPlayer(id, DEFAULT_NAME, 0, socket));
@@ -50,61 +57,6 @@ export class MatchManager {
         this.askForName(this._players[id]);
     }
 
-    public get difficulty(): Difficulty {
-        return this._difficulty;
-    }
-
-    private askForName(player: SPlayer): void {
-        player.socket.emit(msg.requestName);
-    }
-
-    private receiveName(id: number, name: string): void {
-        const player: SPlayer = this.getPlayerById(id);
-        if (player != null) {
-            player.name = name;
-            this.notifyAll(msg.getPlayers, this.Players);
-        }
-    }
-
-    private getPlayerById(id: number): SPlayer {
-        // Check if it's at the right place
-        let player: SPlayer = null;
-        if (this._players[id].id === id) {
-            player = this._players[id];
-        } else {
-            this._players.forEach((p: SPlayer) => {
-                if (p.id === id) { player = p; }
-            });
-        }
-
-        return player;
-    }
-
-    private registerActions(socket: Socket, id: number): void {
-        socket.on(msg.requestName, (name: string) =>
-            this.receiveName(id, name));
-
-        socket.on(msg.playerSelectTile, (letterId: number, orientation: Orientation) =>
-            this.recieveSelect(id, letterId, orientation));
-
-        socket.on(msg.disconnect, () => this.playerLeave(id));
-        socket.on(msg.completedWord, (w: Word) => this.verifyFirst(w, id));
-        socket.on(msg.rematch, () => this.rematch(id));
-    }
-
-    public verifyFirst(w: Word, playerId: number): void {
-        let confirmWord: boolean = true;
-        this.completedWords.forEach((word: Word) => {
-            if (word === w) {
-                confirmWord = false;
-            }
-        });
-        if (confirmWord) {
-            this.completedWords.push(w);
-            this.incerementScore(playerId);
-            this.notifyAll( msg.completedWord, playerId, w);
-        }
-    }
     public notifyOthers(playerId: number, socketMsg: string, ...args: {}[]): void {
         this._players.forEach((p: SPlayer) => {
             if (p.id !== playerId) {
@@ -118,35 +70,95 @@ export class MatchManager {
     }
 
     public get Players(): Array<Player> {
-        return this._players.map((sp: SPlayer) => new Player(sp.id, sp.name, sp.score, sp.wantsRematch));
-    }
-
-    public recieveSelect(playerId: number, letterId: number, orientation: Orientation): void {
-        this.notifyOthers(playerId, msg.playerSelectTile, playerId, letterId, orientation);
-    }
-
-    public playerLeave(id: number): void {
-        this._players.splice(id, 1);
-        this.notifyAll(msg.getPlayers, this.Players);
-
-    }
-
-    public incerementScore(playerId: number): void {
-        this.getPlayerById(playerId).score++;
-        this.notifyAll(msg.getPlayers, this.Players);
+        return this._players.map((sp: SPlayer) =>
+            new Player(sp.id, sp.name, sp.score, sp.wantsRematch)
+        );
     }
 
     public async getNewGrid(): Promise<void> {
-        const link: string = GET_10X10_GRID_LINK + "&difficulty=" + this._difficulty;
+        const link: string =
+        GET_10X10_GRID_LINK + "&difficulty=" + this._difficulty;
         await Request(link, (err: Error, res: Request.FullResponse, grid: CrosswordGrid) =>
-            this.grid = grid);
+            (this.grid = grid));
 
         this.notifyAll(msg.getGrid, this.grid);
     }
 
-    public rematch(id: number): void {
+    private verifyFirst(w: Word, playerId: number): void {
+        let confirmWord: boolean = true;
+        this.completedWords.forEach((word: Word) => {
+            if (word === w) {
+                confirmWord = false;
+            }
+        });
+
+        if (confirmWord) {
+            this.completedWords.push(w);
+            this.incerementScore(playerId);
+            this.notifyAll(msg.completedWord, playerId, w);
+        }
+    }
+
+    private askForName(player: SPlayer): void {
+        player.socket.emit(msg.requestName);
+    }
+
+    private receiveName(id: number, name: string): void {
+        const player: SPlayer = this.getPlayerById(id);
+        if (player != null) {
+        player.name = name;
+        this.notifyAll(msg.getPlayers, this.Players);
+        }
+    }
+
+    private getPlayerById(id: number): SPlayer {
+        // Check if it's at the right place
+        let player: SPlayer = null;
+        if (this._players[id].id === id) {
+        player = this._players[id];
+        } else {
+        this._players.forEach((p: SPlayer) => {
+            if (p.id === id) {
+            player = p;
+            }
+        });
+        }
+
+        return player;
+    }
+
+    private registerActions(socket: Socket, id: number): void {
+        socket.on(msg.requestName, (name: string) => this.receiveName(id, name));
+
+        socket.on(
+        msg.playerSelectTile,
+        (letterId: number, orientation: Orientation) =>
+            this.recieveSelect(id, letterId, orientation)
+        );
+
+        socket.on(msg.disconnect, () => this.playerLeave(id));
+        socket.on(msg.completedWord, (w: Word) => this.verifyFirst(w, id));
+        socket.on(msg.rematch, () => this.rematch(id));
+    }
+
+    private recieveSelect( playerId: number, letterId: number, orientation: Orientation ): void {
+        this.notifyOthers( playerId, msg.playerSelectTile, playerId, letterId, orientation);
+    }
+
+    private playerLeave(id: number): void {
+        this._players.splice(id, 1);
+        this.notifyAll(msg.getPlayers, this.Players);
+    }
+
+    private incerementScore(playerId: number): void {
+        this.getPlayerById(playerId).score++;
+        this.notifyAll(msg.getPlayers, this.Players);
+    }
+
+    private rematch(id: number): void {
         this.getPlayerById(id).wantsRematch = true;
         this.notifyAll(msg.getPlayers, this.Players);
+
         if (this._players.find((player: Player) => !player.wantsRematch) == null) {
             this.completedWords = new Array<Word>();
             this.getNewGrid();
@@ -154,19 +166,18 @@ export class MatchManager {
         }
     }
 
-    public resetPlayers(): void {
+    private resetPlayers(): void {
         this._players.forEach((player: SPlayer) => {
-            player.score = 0;
-            player.wantsRematch = false;
+        player.score = 0;
+        player.wantsRematch = false;
         });
         this.notifyAll(msg.getPlayers, this.Players);
     }
 
     private generateid(): number {
         // Reassign player Ids
-        this._players.forEach((sp: SPlayer, index: number) => sp.id = index);
+        this._players.forEach((sp: SPlayer, index: number) => (sp.id = index));
 
         return this._players.length;
     }
-
 }
