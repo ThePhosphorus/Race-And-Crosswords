723cdf2c77586e989a66d24a77492225bd9490fa
Adem Aber Aouni <ademaberaouni@gmail.com>
2018-04-03 17:58:00 -0400

    Added types for loaded objects

---
+++ client/src/app/race/game-component/loader-service/load-types.enum.ts
@@ -0,0 +1,20 @@
+export enum LoadedObjects {
+    car
+}
+
+export enum LoadedAudios {
+    collision,
+    background_music,
+    car
+}
+
+export enum LoadedTextures {
+    track,
+    start,
+    offRoad
+}
+
+export enum LoaderCubeTextures {
+    daySkyBox,
+    nightSkyBox
+}
--- client/src/app/race/game-component/loader-service/loader-service.service.ts
+++ client/src/app/race/game-component/loader-service/loader-service.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from "@angular/core";
+import { DefaultLoadingManager } from "three";
 
 @Injectable()
 export class LoaderServiceService {
 
-  public constructor() { }
+    public constructor() {}
 
+    public setCallbacks(): void {
+        DefaultLoadingManager.onStart = () => console.log("started The Loading");
+        DefaultLoadingManager.onLoad = () => console.log("loaded");
+        DefaultLoadingManager.onProgress = (item: string, loaded: number, total: number) =>
+            console.log("Loading " + item + ". " + loaded + "/ " + total);
+        DefaultLoadingManager.onError = () => console.error("There was an error while loading");
+    }
 }
