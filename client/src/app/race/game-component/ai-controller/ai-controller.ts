3476dc22fd1c5b858bfa960403056c4f9d4c559f
Randy Sab-Roy <randy.sab.roy@gmail.com>
2018-04-05 13:08:05 -0400

    Find next point

--- client/src/app/race/game-component/ai-controller/ai-controller.ts
+++ client/src/app/race/game-component/ai-controller/ai-controller.ts
@@ -18,8 +18,15 @@ export class AIController extends Object3D {
     }
 
     public update(): void {
-        if (this.track != null && this.getPosition() != null) {
-            this.carControl.accelerate();
+        if (this.track != null) {
+            const objective: Vector3 = this.track[this.findNextPoint()];
+            if (objective.clone().sub(this.getPosition()).length() > 5) {
+                this.carControl.releaseBrakes();
+                this.carControl.accelerate();
+            } else {
+                this.carControl.releaseAccelerator();
+                this.carControl.brake();
+            }
         }
     }
 
@@ -29,5 +36,27 @@ export class AIController extends Object3D {
         }
 
         return new Vector3();
+    }
+
+    private findNextPoint(): number {
+        const pos: Vector3 = this.getPosition();
+        let minDistance: number = Number.MAX_VALUE;
+        let point: number = -1;
+        for (let i: number = 0; i < this.track.length; i++) {
+            const nextIndex: number = (i === this.track.length - 1) ? 1 : i + 1;
+            const p1: Vector3 = this.track[i];
+            const p2: Vector3 = this.track[nextIndex];
+            const distance: number = Math.abs((p2.z - p1.z) * pos.x - (p2.x - p1.x) * pos.z + p2.x * p1.z - p2.z * p1.x) /
+                                              p1.clone().distanceTo(p2);
+            if (distance < minDistance) {
+                const dotProduct: number = p2.clone().sub(p1).dot(pos.clone().sub(p1));
+                if (dotProduct > 1 && dotProduct < p2.clone().sub(p1).lengthSq()) {
+                    point = nextIndex;
+                    minDistance = distance;
+                }
+            }
+        }
+
+        return point;
     }
 }
