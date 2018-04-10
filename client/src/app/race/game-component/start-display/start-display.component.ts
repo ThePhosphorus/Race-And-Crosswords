c140bd219e63efbfb41f34a20a930bb5b4a5385c
Randy Sab-Roy <randy.sab.roy@gmail.com>
2018-04-10 14:23:48 -0400

    Control lights on start

--- client/src/app/race/game-component/start-display/start-display.component.ts
+++ client/src/app/race/game-component/start-display/start-display.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from "@angular/core";
+import { GameManagerService } from "../game-manager-service/game_manager.service";
 
 const NUMBER_OF_LIGHTS: number = 3;
+const TIME_BETWEEN_LEDS: number = 1000;
 
 @Component({
     selector: "app-start-display",
@@ -10,14 +12,43 @@ const NUMBER_OF_LIGHTS: number = 3;
 export class StartDisplayComponent implements OnInit {
 
     public lights: Array<boolean>;
+    private _timer: number;
+    private _hasStarted: boolean;
 
-    public constructor() {
+    public constructor(private gameManagerService: GameManagerService) {
         this.lights = new Array<boolean>();
+        this._timer = 0;
+        this._hasStarted = false;
+
         for (let i: number = 0; i < NUMBER_OF_LIGHTS; i++) {
-            this.lights.push(true);
+            this.lights.push(false);
         }
     }
 
-    public ngOnInit(): void { }
+    public ngOnInit(): void {
+        this.gameManagerService.subscribeToUpdate((deltaTime: number) => this.update(deltaTime));
+    }
+
+    private update(deltaTime: number): void {
+        if (!this._hasStarted) {
+            this._timer += deltaTime;
+
+            for (let i: number = 0; i < this.lights.length; i++) {
+                this.lights[i] = this._timer / TIME_BETWEEN_LEDS > i;
+            }
+
+            if (this._timer > TIME_BETWEEN_LEDS * NUMBER_OF_LIGHTS) {
+                this._hasStarted = true;
+                this.gameManagerService.startGame();
+                this.closeLights();
+            }
+        }
+    }
+
+    private closeLights(): void {
+        for (let i: number = 0; i < this.lights.length; i++) {
+            this.lights[i] = false;
+        }
+    }
 
 }
