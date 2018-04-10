f671a140c946683f2504e216d9c14c5478c344fb
Randy Sab-Roy <randy.sab.roy@gmail.com>
2018-04-10 15:12:43 -0400

    Align lights

--- client/src/app/race/game-component/start-display/start-display.component.ts
+++ client/src/app/race/game-component/start-display/start-display.component.ts
@@ -12,13 +12,13 @@ const TIME_BETWEEN_LEDS: number = 1000;
 export class StartDisplayComponent implements OnInit {
 
     public lights: Array<boolean>;
+    public hasStarted: boolean;
     private _timer: number;
-    private _hasStarted: boolean;
 
     public constructor(private gameManagerService: GameManagerService) {
         this.lights = new Array<boolean>();
         this._timer = 0;
-        this._hasStarted = false;
+        this.hasStarted = false;
 
         for (let i: number = 0; i < NUMBER_OF_LIGHTS; i++) {
             this.lights.push(false);
@@ -30,7 +30,7 @@ export class StartDisplayComponent implements OnInit {
     }
 
     private update(deltaTime: number): void {
-        if (!this._hasStarted) {
+        if (!this.hasStarted) {
             this._timer += deltaTime;
 
             for (let i: number = 0; i < this.lights.length; i++) {
@@ -38,7 +38,7 @@ export class StartDisplayComponent implements OnInit {
             }
 
             if (this._timer > TIME_BETWEEN_LEDS * NUMBER_OF_LIGHTS) {
-                this._hasStarted = true;
+                this.hasStarted = true;
                 this.gameManagerService.startGame();
                 this.closeLights();
             }
