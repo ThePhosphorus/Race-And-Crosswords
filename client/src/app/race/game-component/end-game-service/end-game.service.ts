1f0830bb9fb7035a932971436c78483806a9ae1e
Randy Sab-Roy <randy.sab.roy@gmail.com>
2018-04-12 11:14:52 -0400

    Service can now control game result component

--- client/src/app/race/game-component/end-game-service/end-game.service.ts
+++ client/src/app/race/game-component/end-game-service/end-game.service.ts
@@ -3,6 +3,24 @@ import { Injectable } from "@angular/core";
 @Injectable()
 export class EndGameService {
 
-  public constructor() { }
+    private _displayResult: boolean;
+    private _displayHighscore: boolean;
+
+    public constructor() {
+        this._displayResult = false;
+        this._displayHighscore = false;
+    }
+
+    public get displayResult(): boolean {
+        return this._displayResult;
+    }
+
+    public get displayHighscore(): boolean {
+        return this._displayHighscore;
+    }
+
+    public handleEndGame(): void {
+        this._displayResult = true;
+    }
 
 }
--- client/src/app/race/game-component/game-result-component/game-result-component.component.ts
+++ client/src/app/race/game-component/game-result-component/game-result-component.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { EndGameService } from "../end-game-service/end-game.service";
 
 @Component({
   selector: "app-game-result-component",
@@ -7,7 +8,7 @@ import { Component, OnInit } from "@angular/core";
 })
 export class GameResultComponentComponent implements OnInit {
 
-  public constructor() { }
+  public constructor(public endGameService: EndGameService) {}
 
   public ngOnInit(): void {}
 
