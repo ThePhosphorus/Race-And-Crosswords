d52d3eecd268a9283ef557bec73c2dfcf25986ee
Randy Sab-Roy <randy.sab.roy@gmail.com>
2018-04-12 11:02:49 -0400

    Fix tslint errors

--- client/src/app/race/game-component/end-game-service/end-game-service.service.ts
+++ client/src/app/race/game-component/end-game-service/end-game-service.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
 @Injectable()
 export class EndGameServiceService {
 
-  constructor() { }
+  public constructor() { }
 
 }
