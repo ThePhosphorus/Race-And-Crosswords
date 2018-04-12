c81ae0fcf07f3946a45b764f4371b47b495e6727
Adem Aber Aouni <ademaberaouni@gmail.com>
2018-04-11 20:35:00 -0400

    bezier curves archived

--- client/src/app/race/track-loader/track-mesh-generator.ts
+++ client/src/app/race/track-loader/track-mesh-generator.ts
@@ -1,11 +1,12 @@
 import { Track } from "../../../../../common/race/track";
-import { Vector3, Geometry, Face3, Mesh } from "three";
+import { Vector3, Geometry, Face3, Mesh, CubicBezierCurve3 } from "three";
 import { LengthMismatchException } from "../../exceptions/length-mismatch-exception";
 import { DOUBLE, HALF } from "../../global-constants/constants";
 import { WHITE_MATERIAL } from "../admin/track-editor.constants";
-import { Vector3Struct } from "../../../../../common/race/vector3-struct";
 import { TrackLoaderService } from "./track-loader.service";
 import { DEFAULT_TRACK_WIDTH } from "../race.constants";
+
+const NB_SMOOTHING_VERTECIES: number = 20;
 
 export class TrackMeshGenerator {
     private _length: number;
@@ -15,7 +16,12 @@ export class TrackMeshGenerator {
         this._geometry = new Geometry();
         this._length = 0;
 
+        this.initMeshInfos();
+    }
+
+    private initMeshInfos(): void {
         this.generatePoints();
+        this.smoothEdges();
         this.linkPoints();
     }
 
@@ -28,23 +34,15 @@ export class TrackMeshGenerator {
             const p0: Vector3 = TrackLoaderService.toVector(this._track.points[p0Index]); // Previous
             const p1: Vector3 = TrackLoaderService.toVector(this._track.points[i]);
             const p2: Vector3 = TrackLoaderService.toVector(this._track.points[i + 1]); // Next
-            rightPoints.push(this.getSegmentWall(p0, p1, p2, -1));
-            leftPoints.push(this.getSegmentWall(p0, p1, p2, 1));
+            rightPoints.push(this.getCornerPoint(p0, p1, p2, -1));
+            leftPoints.push(this.getCornerPoint(p0, p1, p2, 1));
         }
 
-        if (leftPoints.length !== rightPoints.length) {
-            throw new LengthMismatchException();
-        } else {
-            this._length = leftPoints.length;
-        }
+        this.fillVertecies(leftPoints, rightPoints);
 
-        for (let i: number = 0; i < this.nbPoints; i++) {
-            this._geometry.vertices.push(leftPoints[i]);
-            this._geometry.vertices.push(rightPoints[i]);
-        }
     }
 
-    private  getSegmentWall(pointP: Vector3, pointA: Vector3, pointB: Vector3, relativeOffset: number): Vector3 {
+    private getCornerPoint(pointP: Vector3, pointA: Vector3, pointB: Vector3, relativeOffset: number): Vector3 {
         const vecPA: Vector3 = pointA.clone().sub(pointP);
         const vecAB: Vector3 = pointB.clone().sub(pointA);
 
@@ -57,7 +55,7 @@ export class TrackMeshGenerator {
                                      pointA.clone().add(perpAB), pointB.clone().add(perpAB));
     }
 
-    private  findIntersection(p1: Vector3, p2: Vector3, p3: Vector3, p4: Vector3): Vector3 {
+    private findIntersection(p1: Vector3, p2: Vector3, p3: Vector3, p4: Vector3): Vector3 {
         if (p2.equals(p3)) {
             return p2.clone();
         }
@@ -77,19 +75,62 @@ export class TrackMeshGenerator {
         return new Vector3(intersectionX, p1.y, intersectionY);
     }
 
+    private smoothEdges(): void {
+        let rightVertecies: Array<Vector3> = new Array<Vector3>();
+        let leftVertecies: Array<Vector3> = new Array<Vector3>();
+
+        for (let i: number = 0; i < this._track.points.length - 1; i++) {
+            const p0Index: number = (i - 1) === -1 ? (this._track.points.length - 2) : (i - 1);
+            const p0: Vector3 = TrackLoaderService.toVector(this._track.points[p0Index]); // Previous
+            const p1: Vector3 = TrackLoaderService.toVector(this._track.points[i]);
+            const p2: Vector3 = TrackLoaderService.toVector(this._track.points[i + 1]); // Next
+
+            rightVertecies = rightVertecies.concat(this.smoothEdge(p1, p2, p0, this.getRightPoint(i)));
+            leftVertecies = leftVertecies.concat(this.smoothEdge(p1, p2, p0, this.getLeftPoint(i)));
+        }
+
+        this.fillVertecies(leftVertecies, rightVertecies);
+    }
+
+    private smoothEdge(pointP: Vector3, pointA: Vector3, pointB: Vector3, edgeId: number): Array<Vector3> {
+        const PB: Vector3 = pointB.clone().sub(pointP).normalize().multiplyScalar(DEFAULT_TRACK_WIDTH);
+        const PA: Vector3 = pointA.clone().sub(pointP).normalize().multiplyScalar(DEFAULT_TRACK_WIDTH);
+
+        const edge: Vector3 = this._geometry.vertices[edgeId];
+
+        const p0: Vector3 = edge.clone().add(PB);
+        const p1: Vector3 = edge.clone().add(PB.clone().multiplyScalar(HALF));
+        const p2: Vector3 = edge.clone().add(PA.clone().multiplyScalar(HALF));
+        const p3: Vector3 = edge.clone().add(PA);
+
+        const arc: CubicBezierCurve3 = new CubicBezierCurve3(p0, p1, p2, p3);
+
+        return arc.getPoints(NB_SMOOTHING_VERTECIES);
+
+    }
+
+    private fillVertecies(leftArray: Array<Vector3>, rightArray: Array<Vector3>): void {
+        if (leftArray.length !== rightArray.length) {
+            throw new LengthMismatchException();
+        } else {
+            this._length = leftArray.length;
+        }
+
+        this._geometry.vertices = new Array<Vector3>();
+
+        for (let i: number = 0; i < this.nbPoints; i++) {
+            this._geometry.vertices.push(leftArray[i]);
+            this._geometry.vertices.push(rightArray[i]);
+        }
+    }
+
     private linkPoints(): void {
 
         const vecUp: Vector3 = new Vector3(0, 1, 0);
         for (let i: number = 0; i < this.nbPoints; i++) {
-            console.log(this.getRightPoint(i));
-
-            console.log("left", this._geometry.vertices[this.getLeftPoint(i)]);
-            console.log("right", this._geometry.vertices[this.getRightPoint(i)]);
-
-            this._geometry.faces.push(new Face3(this.getLeftPoint(i), this.getRightPoint(i + 1), this.getRightPoint(i), vecUp));
-            this._geometry.faces.push(new Face3(this.getLeftPoint(i), this.getLeftPoint(i + 1), this.getRightPoint(i + 1), vecUp));
+            this._geometry.faces.push(new Face3(this.getRightPoint(i), this.getLeftPoint(i + 1), this.getLeftPoint(i), vecUp));
+            this._geometry.faces.push(new Face3(this.getRightPoint(i), this.getRightPoint(i + 1), this.getLeftPoint(i + 1), vecUp));
         }
-        console.log(this._geometry.faces);
 
         this._geometry.computeBoundingSphere();
     }
@@ -98,11 +139,11 @@ export class TrackMeshGenerator {
         return this._length;
     }
 
-    private getLeftPoint(index: number): number {
+    private getRightPoint(index: number): number {
         return DOUBLE * (index % this.nbPoints) + 1;
     }
 
-    private getRightPoint(index: number): number {
+    private getLeftPoint(index: number): number {
         return DOUBLE * (index % this.nbPoints);
     }
 
