af1cad50507753a00c846d8ca3f896d71c0994ae
Unknown <colinste666@gmail.com>
2018-04-13 02:33:18 -0400

    Created tests for definition tile component

--- client/src/app/crossword/definition-tile/definition-tile.component.spec.ts
+++ client/src/app/crossword/definition-tile/definition-tile.component.spec.ts
@@ -1,25 +1,71 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, async } from "@angular/core/testing";
+import { DefinitionTileComponent } from "./definition-tile.component";
+import { CrosswordService } from "../crossword-service/crossword.service";
+import { DisplayedDefinition } from "../definition/definition.component";
+import { Orientation } from "../../../../../common/crossword/enums-constants";
+import { CrosswordCommunicationService } from "../crossword-communication-service/crossword.communication.service";
+import { HttpClientModule } from "@angular/common/http";
+import { By } from "@angular/platform-browser";
 
-import { DefinitionTileComponent } from './definition-tile.component';
+describe("DefinitionTileComponent", () => {
+    let component: DefinitionTileComponent;
+    let fixture: ComponentFixture<DefinitionTileComponent>;
+    let service: CrosswordService;
+    const item: DisplayedDefinition = new DisplayedDefinition("best team", "salads", 0, 0, Orientation.Across);
 
-describe('DefinitionTileComponent', () => {
-  let component: DefinitionTileComponent;
-  let fixture: ComponentFixture<DefinitionTileComponent>;
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [DefinitionTileComponent],
+            providers: [CrosswordService, CrosswordCommunicationService],
+            imports: [HttpClientModule]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ DefinitionTileComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(() => {
+        fixture = TestBed.createComponent(DefinitionTileComponent);
+        component = fixture.componentInstance;
+        component.item = item;
+        component.cheatmode = false;
+        service = TestBed.get(CrosswordService);
+        fixture.detectChanges();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(DefinitionTileComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    it("should create", () => {
+        expect(component).toBeTruthy();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it("should show the definition", () => {
+        expect(fixture.debugElement.query(By.css("p")).nativeElement.textContent).toContain(item.definition);
+    });
+
+    it("should highlight grid tiles on hover", () => {
+        spyOn(service, "setHoveredWord");
+        component.hover();
+        expect(service.setHoveredWord).toHaveBeenCalled();
+    });
+
+    it("should unhighlight grid tiles on unhover", () => {
+        spyOn(service, "setHoveredWord");
+        component.unHover();
+        expect(service.setHoveredWord).toHaveBeenCalled();
+    });
+
+    it("should select grid tiles on click", () => {
+        spyOn(service, "setSelectedWord");
+        component.select(new MouseEvent(""));
+        expect(service.setSelectedWord).toHaveBeenCalled();
+    });
+
+    it("should disable if the word is solved", () => {
+        spyOn(service.gameManager, "isWordSolved").and.returnValue(true);
+        fixture.detectChanges();
+        expect(fixture.debugElement.query(By.css(".disabled"))).not.toBeNull();
+    });
+
+    it("should show the word if the cheatmode is on", () => {
+        component.cheatmode = true;
+        fixture.detectChanges();
+        expect(fixture.debugElement.query(By.css("p")).nativeElement.textContent).toContain(item.word);
+    });
 });
