52e1859f99c00404a5b43274fd5802021bf6d0fe
Adem Aber Aouni <ademaberaouni@gmail.com>
2018-04-18 14:12:54 -0400

    added comment

--- common/race/track.ts
+++ common/race/track.ts
@@ -3,7 +3,7 @@ import { Vector3Struct } from "./vector3-struct";
 
 export class Track {
     public constructor (
-        public _id: string,
+        public _id: string, // le nom de la variable doit être _id, car on utilise le _id de mongoDb.
         public name: string,
         public description: string,
         public points: Vector3Struct[],
